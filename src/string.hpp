0bd4785d19e68109bb5cde58cf8781822c10fa2d
Sainan <63328889+Sainan@users.noreply.github.com>
2022-02-05 17:30:04 +0100

    Add int to hex string functions

--- src/string.hpp
+++ src/string.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cstdint>
+#include <string>
 
 namespace soup
 {
@@ -32,6 +33,31 @@ namespace soup
 			return res;
 		}
 
+		template <typename Str, typename Int, uint8_t Base>
+		[[nodiscard]] static Str from_int_impl_map(Int i, const typename Str::value_type* map)
+		{
+			if (i == 0)
+			{
+				return Str(1, map[0]);
+			}
+			const bool neg = (i < 0);
+			if (neg)
+			{
+				i = i * -1;
+			}
+			Str res{};
+			for (; i != 0; i /= Base)
+			{
+				const auto digit = (i % Base);
+				res.insert(0, 1, map[digit]);
+			}
+			if (neg)
+			{
+				res.insert(0, 1, '-');
+			}
+			return res;
+		}
+
 	public:
 		template <typename Str, typename Int>
 		[[nodiscard]] static Str decimal(Int i)
@@ -43,6 +69,18 @@ namespace soup
 		[[nodiscard]] static Str binary(Int i)
 		{
 			return from_int_impl_ascii<Str, Int, 2>(i);
+		}
+
+		template <typename Int>
+		[[nodiscard]] static std::string hex_upper(Int i)
+		{
+			return from_int_impl_map<std::string, Int, 16>(i, "0123456789ABCDEF");
+		}
+
+		template <typename Int>
+		[[nodiscard]] static std::string hex_lower(Int i)
+		{
+			return from_int_impl_map<std::string, Int, 16>(i, "0123456789abcdef");
 		}
 	};
 }
