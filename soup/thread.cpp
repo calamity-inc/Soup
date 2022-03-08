c79a42bc9a9b95e6e89f4271fe910df51d073f49
Sainan <63328889+Sainan@users.noreply.github.com>
2022-03-08 02:38:04 +0100

    Improve thread

--- soup/thread.cpp
+++ soup/thread.cpp
@@ -1,27 +1,136 @@
 #include "thread.hpp"
 
-#if SOUP_WINDOWS
-
 namespace soup
 {
+	struct capture_thread_ctor_capture
+	{
+		void(*f)(capture&&);
+		capture cap;
+	};
+
+	thread::thread(void(*f)(capture&&), capture&& cap) noexcept
+	{
+		create([](capture&& _cap)
+		{
+			auto& cap = _cap.get<capture_thread_ctor_capture>();
+			cap.f(std::move(cap.cap));
+		}, capture_thread_ctor_capture{
+			f,
+			std::move(cap)
+		});
+	}
+
 	thread::thread(std::function<void()>&& func) noexcept
 		: handle()
 	{
-		auto* fp = new std::function<void()>(std::move(func));
-		this->handle.set(CreateThread(nullptr, 0, [](PVOID handover) -> DWORD
+		create([](capture&& cap)
 		{
-			auto* fp = (std::function<void()>*)handover;
-			(*fp)();
-			delete fp;
-			return 0;
-		}, fp, 0, nullptr));
+			cap.get<std::function<void()>>()();
+		}, std::move(func));
+	}
+
+	struct capture_thread_create
+	{
+		void(*f)(capture&&);
+		capture cap;
+	};
+
+	void thread::create(void(*f)(capture&&), capture&& cap) noexcept
+	{
+		create_capture = capture_thread_create{
+			f,
+			std::move(cap)
+		};
+		osCreate([](void* handover)
+		{
+			auto t = reinterpret_cast<thread*>(handover);
+			auto& cap = t->create_capture.get<capture_thread_create>();
+			cap.f(std::move(cap.cap));
+#if !SOUP_WINDOWS
+			t->running = false;
+#endif
+			t->create_capture.reset();
+		}, this);
+	}
+
+	void thread::osCreate(void(*f)(void*), void* a) noexcept
+	{
+#if SOUP_WINDOWS
+		handle = CreateThread(nullptr, 0, reinterpret_cast<DWORD(*)(void*)>(f), a, 0, nullptr);
+#else
+		pthread_attr_t attr;
+		pthread_attr_init(&attr);
+		//pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+		pthread_create(&handle, &attr, reinterpret_cast<void*(*)(void*)>(f), a);
+#endif
+	}
+
+	thread::~thread() noexcept
+	{
+#if SOUP_WINDOWS
+		TerminateThread(handle, 0);
+		CloseHandle(handle);
+#else
+		if (running)
+		{
+			pthread_cancel(handle);
+			pthread_join(handle, nullptr);
+		}
+#endif
 	}
 
 	bool thread::isRunning() const noexcept
 	{
+#if SOUP_WINDOWS
 		DWORD exit_code;
-		return GetExitCodeThread(handle, &exit_code) && exit_code == STILL_ACTIVE;
+		return GetExitCodeThread(handle, &exit_code)
+			&& exit_code == STILL_ACTIVE
+			;
+#else
+		return running;
+#endif
+	}
+
+	void thread::stop() noexcept
+	{
+#if SOUP_WINDOWS
+		TerminateThread(handle, 0);
+#else
+		if (running)
+		{
+			pthread_cancel(handle);
+			pthread_join(handle, nullptr);
+			running = false;
+		}
+#endif
+	}
+
+	void thread::awaitCompletion() const noexcept
+	{
+#if SOUP_WINDOWS
+		WaitForSingleObject(handle, INFINITE);
+#else
+		if (running)
+		{
+			pthread_join(handle, nullptr);
+		}
+#endif
+	}
+
+	void thread::awaitCompletion(const std::vector<thread>& threads) noexcept
+	{
+#if SOUP_WINDOWS
+		std::vector<HANDLE> handles{};
+		for (const auto& t : threads)
+		{
+			handles.emplace_back(t.handle);
+		}
+		WaitForMultipleObjects((DWORD)handles.size(), handles.data(), TRUE, INFINITE);
+#else
+		for (const auto& t : threads)
+		{
+			t.awaitCompletion();
+		}
+#endif
 	}
 }
-
-#endif
--- soup/thread.hpp
+++ soup/thread.hpp
@@ -1,23 +1,46 @@
 #pragma once
 
 #include "base.hpp"
-#if SOUP_WINDOWS
 
-#include "handle_raii.hpp"
+#if SOUP_WINDOWS
+#include <Windows.h>
+#else
+#include <pthread.h>
+#endif
 
 #include <functional>
+#include <vector>
+
+#include "capture.hpp"
 
 namespace soup
 {
 	class thread
 	{
 	public:
-		handle_raii handle;
+#if SOUP_WINDOWS
+		HANDLE handle;
+#else
+		pthread_t handle;
+		bool running = true;
+#endif
+		capture create_capture;
 
+		explicit thread(void(*f)(capture&&), capture&& cap) noexcept;
 		explicit thread(std::function<void()>&& func) noexcept;
+		explicit thread(const thread& b) = delete;
+		explicit thread(thread&& b) = delete;
+	private:
+		void create(void(*f)(capture&&), capture&& cap) noexcept;
+		void osCreate(void(*f)(void*), void* a = nullptr) noexcept;
+
+	public:
+		~thread() noexcept;
 
 		[[nodiscard]] bool isRunning() const noexcept;
+		void stop() noexcept;
+
+		void awaitCompletion() const noexcept;
+		static void awaitCompletion(const std::vector<thread>& threads) noexcept;
 	};
 }
-
-#endif
