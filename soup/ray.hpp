67736d47dfe578de4e6642d9bb671168b906c0aa
Sainan <63328889+Sainan@users.noreply.github.com>
2022-04-11 12:14:17 +0200

    Add ray direction construction

--- soup/ray.hpp
+++ soup/ray.hpp
@@ -15,9 +15,19 @@ namespace soup
 		{
 		}
 
-		ray(const vector3& start, const vector3& rotation, float length) noexcept
+		[[deprecated]] ray(const vector3& start, const vector3& rotation, float length) noexcept
 			: start(start), end(start + (rotation.toDir() * length))
 		{
+		}
+
+		[[nodiscard]] static ray withRot(const vector3& start, const vector3& rot, float len)
+		{
+			return withDir(start, rot.toDir(), len);
+		}
+
+		[[nodiscard]] static ray withDir(const vector3& start, const vector3& dir, float len)
+		{
+			return ray{ start, start + (dir * len) };
 		}
 	};
 #pragma pack(pop)
