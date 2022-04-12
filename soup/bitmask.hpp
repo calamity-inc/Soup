51e19b9426c9b36fe53bcbfcb40b4bbcf203389d
Sainan <63328889+Sainan@users.noreply.github.com>
2022-04-12 06:59:28 +0200

    Add bitmask

---
+++ soup/bitmask.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+namespace soup
+{
+	struct bitmask
+	{
+		template <typename T>
+		[[nodiscard]] static constexpr T generateLo(uint8_t width)
+		{
+			constexpr auto T_width = (sizeof(T) * 8);
+			if (width == T_width)
+			{
+				T bitmask = 0;
+				for (uint8_t i = 0; i != width; ++i)
+				{
+					bitmask |= (((T)1) << i);
+				}
+				return bitmask;
+			}
+			return (((T)1) << width) - 1;
+		}
+
+		template <typename T>
+		[[nodiscard]] static constexpr T generateHi(uint8_t width)
+		{
+			constexpr auto T_width = (sizeof(T) * 8);
+			if (width == T_width)
+			{
+				T bitmask = 0;
+				for (uint8_t i = T_width - 1; width != 0; --i, --width)
+				{
+					bitmask |= (((T)1) << i);
+				}
+				return bitmask;
+			}
+			return ((((T)1) << width) - 1) << (T_width - width);
+		}
+	};
+
+	static_assert(bitmask::generateLo<uint32_t>(22) == 0b1111111111111111111111);
+	static_assert(bitmask::generateLo<uint64_t>(22) == 0b1111111111111111111111);
+
+	static_assert(bitmask::generateHi<uint32_t>(22) == 0b11111111111111111111110000000000);
+}
