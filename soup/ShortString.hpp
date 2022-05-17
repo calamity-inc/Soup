d5dd66698e5c717e9b5c5b0570fcf201ce6b6388
Sainan <63328889+Sainan@users.noreply.github.com>
2022-05-17 16:28:24 +0200

    Add ShortString

---
+++ soup/ShortString.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <cstring>
+
+namespace soup
+{
+	template <size_t S>
+	class ShortString
+	{
+	private:
+		char m_data[S] = { 0 };
+		const char term = 0;
+
+	public:
+		ShortString() noexcept
+		{
+		}
+
+		ShortString(const std::string& b) noexcept
+		{
+			operator=(b);
+		}
+
+		ShortString(const char* b) noexcept
+		{
+			operator=(b);
+		}
+
+		void operator =(const std::string& b) noexcept
+		{
+			operator=(b.c_str());
+		}
+
+		void operator =(const char* b) noexcept
+		{
+			strncpy(data(), b, S);
+		}
+
+		[[nodiscard]] char* data() noexcept
+		{
+			return &m_data[0];
+		}
+
+		[[nodiscard]] const char* data() const noexcept
+		{
+			return &m_data[0];
+		}
+
+		[[nodiscard]] const char* c_str() const noexcept
+		{
+			return &m_data[0];
+		}
+
+		[[nodiscard]] static size_t capacity() noexcept
+		{
+			return S;
+		}
+
+		[[nodiscard]] size_t size() const noexcept
+		{
+			return strlen(c_str());
+		}
+	};
+	static_assert(sizeof(ShortString<1>) == 2);
+	static_assert(sizeof(ShortString<2>) == 3);
+	static_assert(sizeof(ShortString<3>) == 4);
+	static_assert(sizeof(ShortString<4>) == 5);
+	static_assert(sizeof(ShortString<5>) == 6);
+	static_assert(sizeof(ShortString<6>) == 7);
+	static_assert(sizeof(ShortString<7>) == 8);
+	static_assert(sizeof(ShortString<8>) == 9);
+	static_assert(sizeof(ShortString<9>) == 10);
+}
