b9c509834dcdfeb2057dc9446c96d71f3b930151
Sainan <63328889+Sainan@users.noreply.github.com>
2022-06-03 16:22:16 +0200

    Add Fiber

---
+++ soup/Fiber.cpp
@@ -0,0 +1,88 @@
+#include "Fiber.hpp"
+
+#if SOUP_WINDOWS
+#include <Windows.h>
+#endif
+
+namespace soup
+{
+#if SOUP_WINDOWS
+	__stdcall void Fiber::entry(void* _f)
+#else
+	void Fiber::entry()
+#endif
+	{
+#if SOUP_WINDOWS
+		Fiber* f = reinterpret_cast<Fiber*>(_f);
+#else
+		Fiber* f = Fiber::current();
+#endif
+		f->func(std::move(f->cap));
+
+		f->func = nullptr;
+		do
+		{
+			f->yield();
+		} while (true);
+	}
+
+	Fiber::Fiber(func_t func, Capture&& cap) noexcept
+		: func(func), cap(std::move(cap))
+#if SOUP_WINDOWS
+		, inst(CreateFiber(0, &entry, this))
+#endif
+	{
+#if SOUP_LINUX
+		getcontext(&ctx);
+		ctx.uc_link = &ret_ctx;
+		ctx.uc_stack.ss_sp = stack;
+		ctx.uc_stack.ss_size = sizeof(stack);
+		makecontext(&ctx, &entry, 0);
+#endif
+	}
+
+	Fiber::~Fiber() noexcept
+	{
+#if SOUP_WINDOWS
+		DeleteFiber(inst);
+#endif
+	}
+
+	Fiber* Fiber::current() noexcept
+	{
+#if SOUP_WINDOWS
+		return static_cast<Fiber*>(GetFiberData());
+#else
+		return _current;
+#endif
+	}
+
+	void Fiber::yield() noexcept
+	{
+#if SOUP_WINDOWS
+		SwitchToFiber(return_to);
+#else
+		swapcontext(&ctx, &ret_ctx);
+#endif
+	}
+
+	void Fiber::run() noexcept
+	{
+#if SOUP_WINDOWS
+		if (!IsThreadAFiber())
+		{
+			ConvertThreadToFiber(nullptr);
+		}
+		return_to = GetCurrentFiber();
+		SwitchToFiber(inst);
+#else
+		_current = this;
+		swapcontext(&ret_ctx, &ctx);
+#endif
+	}
+
+	bool Fiber::hasFinished() const noexcept
+	{
+		return func == nullptr;
+	}
+}
---
+++ soup/Fiber.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "base.hpp"
+#include "Capture.hpp"
+
+#if SOUP_LINUX
+#include <ucontext.h>
+#include <signal.h>
+#endif
+
+namespace soup
+{
+	class Fiber
+	{
+	public:
+		using func_t = void(*)(Capture&&);
+
+	private:
+		func_t func;
+		Capture cap;
+#if SOUP_WINDOWS
+		void* inst;
+		void* return_to;
+#else
+		ucontext_t ctx;
+		ucontext_t ret_ctx;
+		char stack[SIGSTKSZ];
+
+		inline static thread_local Fiber* _current;
+#endif
+
+#if SOUP_WINDOWS
+		static __stdcall void entry(void*);
+#else
+		static void entry();
+#endif
+
+	public:
+		explicit Fiber(func_t func, Capture&& cap = {}) noexcept;
+		~Fiber() noexcept;
+
+		[[nodiscard]] static Fiber* current() noexcept;
+		void yield() noexcept;
+
+		void run() noexcept;
+		[[nodiscard]] bool hasFinished() const noexcept;
+	};
+}
