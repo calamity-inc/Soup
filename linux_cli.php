540b56f0273daf60f6fed873eb9712e6f0c84e5c
Sainan <63328889+Sainan@users.noreply.github.com>
2022-06-11 22:39:51 +0200

    Require lld to build cli

--- linux_cli.php
+++ linux_cli.php
@@ -1,11 +1,6 @@
 <?php
 require "linux_config.php";
 
-$clang .= " -fuse-ld=";
-if(shell_exec("which lld"))
-{
-	$clang .= "l";
-}
-$clang .= "ld -lstdc++ -lstdc++fs -pthread -lresolv -lm";
+$clang .= " -fuse-ld=lld -lstdc++ -lstdc++fs -pthread -lresolv -lm";
 
 passthru("$clang \"CLI/cli.cpp\" \"CLI/cli_3d.cpp\" \"CLI/cli_repl.cpp\" libsoup.a -I soup -o \"soupcli\"");
