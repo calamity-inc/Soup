e710ceb9b4c15239c696bd7ebd263642e187f7dd
Sainan <63328889+Sainan@users.noreply.github.com>
2022-06-23 06:06:13 +0200

    Add Searchspace

---
+++ soup/Searchspace.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+namespace soup
+{
+	struct Searchspace
+	{
+		struct Range
+		{
+			size_t begin;
+			size_t end;
+		};
+
+		// begin = 0
+		size_t end;
+
+		[[nodiscard]] std::vector<Range> split(size_t parts) const
+		{
+			if (parts == 1)
+			{
+				return { Range{ 0, end } };
+			}
+			std::vector<Range> res{};
+			res.reserve(parts);
+			size_t part_size = (end / parts);
+			size_t coverage = (part_size * parts);
+			--parts;
+			for (size_t i = 0; i != parts; ++i)
+			{
+				res.emplace_back(Range{ i * part_size, part_size });
+			}
+			res.emplace_back(Range{ parts * part_size, part_size + (end - coverage) });
+			return res;
+		}
+	};
+}
