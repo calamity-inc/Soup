9f49eda2d4c105cc4a8b7833898cdfc6fb2edea2
Sainan <63328889+Sainan@users.noreply.github.com>
2022-06-29 15:35:20 +0200

    Turn php test into a unit with smaller tests

--- CLI/cli_test.cpp
+++ CLI/cli_test.cpp
@@ -77,23 +77,47 @@ void tests()
 		assert(obj->at("spouse")->isNull());
 	});
 
-	test("php", []
+	unit("php")
 	{
-		PhpState php;
-		assert(php.evaluate(R"(<?php echo "Hello";)") == "Hello");
-		assert(php.evaluate(R"(<?php echo 123;)") == "123");
-		assert(php.evaluate(R"(<?php $a = 1; echo $a;)") == "1");
-		assert(php.evaluate(R"(<?php $a = "Hello"; echo $a;)") == "Hello");
-		assert(php.evaluate(R"(<?php $a = "Hello, "."world!"; echo $a;)") == "Hello, world!");
-		assert(php.evaluate(R"(<?php $a = function(){ echo "Hello"; }; $a();)") == "Hello");
-		assert(php.evaluate(R"(<?php $a = function(){ ?>Hello<?php }; $a();)") == "Hello");
-		assert(php.evaluate(R"(<?php $a = function(){ ?>Hello<?php };)") == "");
-		assert(php.evaluate(R"(<?php $a = function(){ $a = "Hello"; echo $a; }; $a();)") == "Hello");
-		assert(php.evaluate(R"(<?php
+		test("echo", []
+		{
+			PhpState php;
+			assert(php.evaluate(R"(<?php echo "Hello";)") == "Hello");
+			assert(php.evaluate(R"(<?php echo 123;)") == "123");
+		});
+		test("variables", []
+		{
+			PhpState php;
+			assert(php.evaluate(R"(<?php $a = "Hello"; echo $a;)") == "Hello");
+			assert(php.evaluate(R"(<?php $a = 1; echo $a;)") == "1");
+		});
+		test("concat", []
+		{
+			PhpState php;
+			assert(php.evaluate(R"(<?php $a = "Hello"; echo $a;)") == "Hello");
+			assert(php.evaluate(R"(<?php $a = "Hello, "."world!"; echo $a;)") == "Hello, world!");
+		});
+		test("functions", []
+		{
+			PhpState php;
+			assert(php.evaluate(R"(<?php $a = function(){ echo "Hello"; }; $a();)") == "Hello");
+			assert(php.evaluate(R"(<?php $a = function(){ ?>Hello<?php }; $a();)") == "Hello");
+			assert(php.evaluate(R"(<?php $a = function(){ ?>Hello<?php };)") == "");
+			assert(php.evaluate(R"(<?php $a = function(){ $a = "Hello"; echo $a; }; $a();)") == "Hello");
+		});
+		test("comments", []
+		{
+			PhpState php;
+			assert(php.evaluate(R"(<?php
 //echo "Hello";
 echo "Hello, world!";)") == "Hello, world!");
-		assert(php.evaluate(R"(<?php echo 1 == 1;)") == "1"); // this is what vanilla PHP also prints, but "true" would be nicer
-	});
+		});
+		test("comparisons", []
+		{
+			PhpState php;
+			assert(php.evaluate(R"(<?php echo 1 == 1;)") == "1"); // this is what vanilla PHP also prints, but "true" would be nicer
+		});
+	}
 }
 
 void cli_test()
