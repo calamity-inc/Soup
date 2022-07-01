fbe2e5c25dc8fb396e2250138d48bf0704585c6c
Sainan <63328889+Sainan@users.noreply.github.com>
2022-07-01 16:02:16 +0200

    Add M_TAU

--- soup/math.hpp
+++ soup/math.hpp
@@ -3,5 +3,7 @@
 #define _USE_MATH_DEFINES
 #include <math.h>
 
+#define M_TAU (M_PI * 2.0)
+
 #define DEG_TO_RAD(deg) ((deg) * (float(M_PI) / 180.0f))
 #define DEG_TO_HALF_RAD(deg) ((deg) * (float(M_PI) / 360.0f))
