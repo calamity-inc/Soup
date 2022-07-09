5c0f9177f26d8ef61f1f350700b52dcdcd698725
Sainan <63328889+Sainan@users.noreply.github.com>
2022-07-10 00:20:59 +0200

    Add Timer

---
+++ soup/Timer.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "time.hpp"
+
+namespace soup
+{
+	class Timer
+	{
+	private:
+		std::time_t started_at;
+
+	public:
+		inline Timer() noexcept
+		{
+			start();
+		}
+
+		inline void start() noexcept
+		{
+			started_at = time::nanos();
+		}
+
+		inline void stop() noexcept // returns ms
+		{
+			started_at = (time::nanos() - started_at);
+		}
+
+		[[nodiscard]] inline std::time_t getNanos() noexcept
+		{
+			return started_at;
+		}
+
+		[[nodiscard]] inline double getMs() noexcept
+		{
+			return getNanos() / 1000000.0;
+		}
+	};
+}
