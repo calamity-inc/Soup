07ca0ec55a9ab9ed87b2884e604e6b2a1a05e084
Sainan <63328889+Sainan@users.noreply.github.com>
2022-08-01 15:12:56 +0200

    Update default z_near

--- soup/SceneRasterisingRenderer.hpp
+++ soup/SceneRasterisingRenderer.hpp
@@ -9,7 +9,7 @@ namespace soup
 
 	struct SceneRasterisingRenderer : public SceneRenderer
 	{
-		float z_near = 0.1f;
+		float z_near = 0.00001f;
 		float z_far = 1000.0f;
 
 		void render(const Scene& s, RenderTarget& rt, float fov) const final;
