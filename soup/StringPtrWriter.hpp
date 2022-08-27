c4077163a7a2ef335bcc4e0cd87a8e79326cb92d
Sainan <63328889+Sainan@users.noreply.github.com>
2022-08-27 08:07:20 +0200

    Add StringPtrWriter

---
+++ soup/StringPtrWriter.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Writer.hpp"
+
+namespace soup
+{
+	class StringPtrWriter final : public Writer
+	{
+	public:
+		std::string* str;
+
+		StringPtrWriter(std::string* str, bool little_endian = true)
+			: Writer(little_endian), str(str)
+		{
+		}
+
+		~StringPtrWriter() final = default;
+
+	protected:
+		void write(const char* data, size_t size) final
+		{
+			str->append(data, size);
+		}
+	};
+}
