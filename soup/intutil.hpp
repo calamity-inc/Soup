02bd2f665c602c9f473297e093b904573fc72efc
Sainan <63328889+Sainan@users.noreply.github.com>
2022-09-06 18:42:45 +0200

    Use compiler builtin for invertEndianness if available

--- soup/intutil.hpp
+++ soup/intutil.hpp
@@ -7,15 +7,22 @@ namespace soup
 		// C++23 will add std::byteswap
 		[[nodiscard]] static constexpr uint32_t invertEndianness(uint32_t val)
 		{
+#if defined(__GNUC__) || defined(__clang__)
+			return __builtin_bswap32(val);
+#else
 			return val << (32 - 8)
 				| ((val >> 8) & 0xFF) << (32 - 16)
 				| ((val >> 16) & 0xFF) << (32 - 24)
 				| ((val >> 24) & 0xFF)
 				;
+#endif
 		}
 		
 		[[nodiscard]] static constexpr uint64_t invertEndianness(uint64_t val)
 		{
+#if defined(__GNUC__) || defined(__clang__)
+			return __builtin_bswap64(val);
+#else
 			return val << (64 - 8)
 				| ((val >> 8) & 0xFF) << (64 - 16)
 				| ((val >> 16) & 0xFF) << (64 - 24)
@@ -25,6 +32,7 @@ namespace soup
 				| ((val >> 48) & 0xFF) << (64 - 56)
 				| ((val >> 56) & 0xFF)
 				;
+#endif
 		}
 
 		template <typename T>
