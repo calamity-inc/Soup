40b748a38f234c9bc3f1cdf162f5312ce0b2888c
Sainan <63328889+Sainan@users.noreply.github.com>
2022-09-14 23:58:20 +0200

    Add Timer

---
+++ soup/Timer.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "time.hpp"
+
+namespace soup
+{
+	class Timer
+	{
+	private:
+		std::time_t deadline = 0;
+
+	public:
+		void start(std::time_t ms) noexcept
+		{
+			deadline = time::millis() + ms;
+		}
+
+		[[nodiscard]] bool isActive() const noexcept
+		{
+			return deadline != 0;
+		}
+
+		void reset() noexcept
+		{
+			deadline = 0;
+		}
+
+		[[nodiscard]] bool hasEnded() const noexcept
+		{
+			return time::millis() >= deadline;
+		}
+	};
+}
