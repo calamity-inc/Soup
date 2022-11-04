6719cadc6d263d0e6c8bd69950be297615814fb6
Sainan <63328889+Sainan@users.noreply.github.com>
2022-11-04 10:26:08 +0100

    Add netInfo::getPublicAddress

---
+++ soup/netInfo.cpp
@@ -0,0 +1,32 @@
+#include "netInfo.hpp"
+
+#include "HttpRequest.hpp"
+#include "IpAddr.hpp"
+
+namespace soup
+{
+	IpAddr netInfo::getPublicAddress()
+	{
+		return getPublicAddressImpl("ip.apimon.de");
+	}
+
+	IpAddr netInfo::getPublicAddressV4()
+	{
+		return getPublicAddressImpl("ipv4.apimon.de");
+	}
+
+	IpAddr netInfo::getPublicAddressV6()
+	{
+		return getPublicAddressImpl("ipv6.apimon.de");
+	}
+
+	IpAddr netInfo::getPublicAddressImpl(const std::string& provider)
+	{
+		HttpRequest req(provider, "/");
+		if (auto res = req.execute(); res.has_value())
+		{
+			return IpAddr(res->body);
+		}
+		return IpAddr();
+	}
+}
---
+++ soup/netInfo.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "fwd.hpp"
+
+#include <string>
+
+namespace soup
+{
+	class netInfo
+	{
+	public:
+		// Requests the device's internet address as seen by another device, or [::] if unavailable.
+		[[nodiscard]] static IpAddr getPublicAddress();
+		[[nodiscard]] static IpAddr getPublicAddressV4();
+		[[nodiscard]] static IpAddr getPublicAddressV6();
+	protected:
+		[[nodiscard]] static IpAddr getPublicAddressImpl(const std::string& provider);
+	};
+}
