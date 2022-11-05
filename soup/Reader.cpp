b4f63d03adbb82de725c29128d91e21259bce432
Sainan <63328889+Sainan@users.noreply.github.com>
2022-11-05 10:37:08 +0100

    Fix Reader::om_bigint on 32-bit targets

--- soup/Reader.cpp
+++ soup/Reader.cpp
@@ -6,7 +6,20 @@ namespace soup
 {
 	bool Reader::om_bigint(Bigint& v)
 	{
-		return om<Bigint>(v);
+		Bigint val{};
+		while (hasMore())
+		{
+			uint8_t byte;
+			u8(byte);
+			val <<= 7;
+			val |= (Bigint::chunk_t)(byte & 0x7F);
+			if (!(byte & 0x80))
+			{
+				break;
+			}
+		}
+		v = val;
+		return true;
 	}
 
 	bool Reader::bigint_lp_u64_dyn(Bigint& v)
