aa63826985dedf4af8ad97d7582f7da3577dbea9
Sainan <63328889+Sainan@users.noreply.github.com>
2022-11-20 23:29:54 +0100

    Add soup::strong_ordering & SOUP_SPACESHIP to allow for more compatible code

---
+++ soup/spaceship.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "base.hpp"
+
+#define SOUP_SPACESHIP_USE_STD SOUP_CPP20
+
+#if SOUP_SPACESHIP_USE_STD
+#include <compare>
+#endif
+
+namespace soup
+{
+#if SOUP_SPACESHIP_USE_STD
+	using strong_ordering = ::std::strong_ordering;
+
+	#define SOUP_SPACESHIP(a, b) ((a) <=> (b))
+#else
+	class strong_ordering
+	{
+	private:
+		int value;
+
+	public:
+		static const strong_ordering less;
+		static const strong_ordering equal;
+		static const strong_ordering greater;
+
+		constexpr strong_ordering(int value) noexcept
+			: value(value)
+		{
+		}
+
+		constexpr operator int() const noexcept
+		{
+			return value;
+		}
+	};
+
+	template <typename T>
+	[[nodiscard]] strong_ordering SOUP_SPACESHIP(const T& a, const T& b)
+	{
+		if (a == b)
+		{
+			return strong_ordering::equal;
+		}
+		if (a < b)
+		{
+			return strong_ordering::less;
+		}
+		return strong_ordering::greater;
+	}
+#endif
+}
