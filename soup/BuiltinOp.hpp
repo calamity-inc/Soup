d210bf71d8d323e849ae1521ca31040642ad25ec
Sainan <63328889+Sainan@users.noreply.github.com>
2022-11-22 02:21:58 +0100

    Improve formatting

--- soup/BuiltinOp.hpp
+++ soup/BuiltinOp.hpp
@@ -4,11 +4,11 @@ namespace soup
 {
 	enum BuiltinOp : uint8_t
 	{
-		OP_PUSH_STR = 0xFF - 0,
-		OP_PUSH_INT = 0xFF - 1,
+		OP_PUSH_STR  = 0xFF - 0,
+		OP_PUSH_INT  = 0xFF - 1,
 		OP_PUSH_UINT = 0xFF - 2,
-		OP_PUSH_FUN = 0xFF - 3,
-		OP_PUSH_VAR = 0xFF - 4,
-		OP_POP_ARGS = 0xFF - 5,
+		OP_PUSH_FUN  = 0xFF - 3,
+		OP_PUSH_VAR  = 0xFF - 4,
+		OP_POP_ARGS  = 0xFF - 5,
 	};
 }
