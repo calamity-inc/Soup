7ace28dfb003060b854d83375fc362f886ca7bad
Sainan <63328889+Sainan@users.noreply.github.com>
2022-11-23 23:57:45 +0100

    Add format_expandLiteralPart

--- soup/format.hpp
+++ soup/format.hpp
@@ -29,23 +29,8 @@ namespace soup
 		return v.toString();
 	}
 
-	template <typename T>
-	void format_expandArg(std::string& res, size_t& sep, const std::string& str, const T& arg)
+	inline void format_expandLiteralPart(std::string& res, size_t& sep, const std::string& str)
 	{
-		if (sep == std::string::npos) // More arguments than instances of "{}"?
-		{
-			return;
-		}
-
-		if constexpr (std::is_same_v<T, std::string>)
-		{
-			res.append(arg);
-		}
-		else
-		{
-			res.append(format_toString<T>(arg));
-		}
-
 		sep = str.find('}', sep);
 		if (sep != std::string::npos)
 		{
@@ -63,6 +48,26 @@ namespace soup
 		}
 	}
 
+	template <typename T>
+	void format_expandArg(std::string& res, size_t& sep, const std::string& str, const T& arg)
+	{
+		if (sep == std::string::npos) // More arguments than instances of "{}"?
+		{
+			return;
+		}
+
+		if constexpr (std::is_same_v<T, std::string>)
+		{
+			res.append(arg);
+		}
+		else
+		{
+			res.append(format_toString<T>(arg));
+		}
+
+		format_expandLiteralPart(res, sep, str);
+	}
+
 	template <typename...Args>
 	std::string format(const std::string& str, const Args&... args)
 	{
@@ -73,7 +78,7 @@ namespace soup
 
 		while (sep != std::string::npos) // More instances of "{}" than arguments?
 		{
-			format_expandArg(res, sep, str, std::string{});
+			format_expandLiteralPart(res, sep, str);
 		}
 
 		return res;
