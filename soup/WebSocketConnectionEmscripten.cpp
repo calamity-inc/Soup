872bdc5acab38857937edae444cb52c281915f7a
Sainan <63328889+Sainan@users.noreply.github.com>
2022-11-30 12:59:18 +0100

    Rename WebSocketConnection to WebSocketConnectionEmscripten

--- soup/WebSocketConnectionEmscripten.cpp
+++ soup/WebSocketConnectionEmscripten.cpp
@@ -1,10 +1,10 @@
-#include "WebSocketConnection.hpp"
+#include "WebSocketConnectionEmscripten.hpp"
 
 #if SOUP_WASM || SOUP_CODE_INSPECTOR
 
 namespace soup
 {
-	WebSocketConnection::WebSocketConnection(const std::string& url)
+	WebSocketConnectionEmscripten::WebSocketConnectionEmscripten(const std::string& url)
 	{
 		EmscriptenWebSocketCreateAttributes ws_attrs;
 		emscripten_websocket_init_create_attributes(&ws_attrs);
@@ -15,40 +15,40 @@ namespace soup
 
 	static EM_BOOL em_onopen(int eventType, const EmscriptenWebSocketOpenEvent* e, void* userData)
 	{
-		WebSocketConnection con{ e->socket };
-		reinterpret_cast<WebSocketConnection::on_open_t>(userData)(con);
+		WebSocketConnectionEmscripten con{ e->socket };
+		reinterpret_cast<WebSocketConnectionEmscripten::on_open_t>(userData)(con);
 		return EM_TRUE;
 	}
 
-	void WebSocketConnection::onOpen(on_open_t f)
+	void WebSocketConnectionEmscripten::onOpen(on_open_t f)
 	{
 		emscripten_websocket_set_onopen_callback(ws, reinterpret_cast<void*>(f), &em_onopen);
 	}
 
 	static EM_BOOL em_onmessage(int eventType, const EmscriptenWebSocketMessageEvent* e, void* userData)
 	{
-		WebSocketConnection con{ e->socket };
+		WebSocketConnectionEmscripten con{ e->socket };
 		WebSocketMessage msg{
 			std::string((const char*)e->data, (size_t)e->numBytes),
 			(bool)e->isText
 		};
-		reinterpret_cast<WebSocketConnection::on_message_t>(userData)(con, msg);
+		reinterpret_cast<WebSocketConnectionEmscripten::on_message_t>(userData)(con, msg);
 		return EM_TRUE;
 	}
 
-	void WebSocketConnection::onMessage(on_message_t f)
+	void WebSocketConnectionEmscripten::onMessage(on_message_t f)
 	{
 		emscripten_websocket_set_onmessage_callback(ws, reinterpret_cast<void*>(f), &em_onmessage);
 	}
 
 	static EM_BOOL em_onclose(int eventType, const EmscriptenWebSocketCloseEvent* e, void* userData)
 	{
-		WebSocketConnection con{ e->socket };
-		reinterpret_cast<WebSocketConnection::on_close_t>(userData)(con);
+		WebSocketConnectionEmscripten con{ e->socket };
+		reinterpret_cast<WebSocketConnectionEmscripten::on_close_t>(userData)(con);
 		return EM_TRUE;
 	}
 
-	void WebSocketConnection::onClose(on_close_t f)
+	void WebSocketConnectionEmscripten::onClose(on_close_t f)
 	{
 		emscripten_websocket_set_onclose_callback(ws, reinterpret_cast<void*>(f), &em_onclose);
 	}
--- soup/WebSocketConnectionEmscripten.hpp
+++ soup/WebSocketConnectionEmscripten.hpp
@@ -15,20 +15,20 @@ namespace soup
 	// - https://gist.github.com/nus/564e9e57e4c107faa1a45b8332c265b9
 	// Need to link with -lwebsocket.js
 
-	class WebSocketConnection
+	class WebSocketConnectionEmscripten
 	{
 	public:
-		using on_open_t = void(*)(WebSocketConnection&);
-		using on_message_t = void(*)(WebSocketConnection&, const WebSocketMessage&);
-		using on_close_t = void(*)(WebSocketConnection&);
+		using on_open_t = void(*)(WebSocketConnectionEmscripten&);
+		using on_message_t = void(*)(WebSocketConnectionEmscripten&, const WebSocketMessage&);
+		using on_close_t = void(*)(WebSocketConnectionEmscripten&);
 
 	private:
 		EMSCRIPTEN_WEBSOCKET_T ws;
 
 	public:
-		explicit WebSocketConnection(const std::string& url); // url e.g. wss://echo.websocket.org
+		explicit WebSocketConnectionEmscripten(const std::string& url); // url e.g. wss://echo.websocket.org
 
-		explicit WebSocketConnection(EMSCRIPTEN_WEBSOCKET_T ws)
+		explicit WebSocketConnectionEmscripten(EMSCRIPTEN_WEBSOCKET_T ws)
 			: ws(ws)
 		{
 		}
