a188cb66c82772deb7cb2d8f669025d4ef051a24
Alessandro <66976091+alessandromrc@users.noreply.github.com>
2022-12-26 00:20:02 +0100

    Added Anexia Hosting
    More infos about Anexia:
    https://www.peeringdb.com/net/3241
    
    Example IP:
    
    https://iphub.info/?ip=131.100.2.0

--- soup/netAs.cpp
+++ soup/netAs.cpp
@@ -94,6 +94,7 @@ namespace soup
 		case 47674: // related to BLAZINGFAST
 		case 206444: // KUBBUR
 		case 140952: // Strong Technology, LLC
+		case 42473: // ANEXIA Internetdienstleistungs GmbH		
 			// Green Floid LLC
 		case 40622:
 		case 204957:
