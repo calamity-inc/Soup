3661923f10e2c5b6dd1408170e1ca70bc6100997
Sainan <63328889+Sainan@users.noreply.github.com>
2023-01-03 09:31:29 +0100

    Add TreeReader::toString

--- soup/TreeReader.hpp
+++ soup/TreeReader.hpp
@@ -10,5 +10,28 @@ namespace soup
 		[[nodiscard]] virtual std::string getValue(const void* node) const = 0;
 		[[nodiscard]] virtual bool hasChildren(const void* node) const = 0;
 		[[nodiscard]] virtual std::vector<const void*> getChildren(const void* node) const = 0;
+
+		[[nodiscard]] std::string toString(const void* root, const std::string& prefix = {}) const
+		{
+			std::string str;
+			for (const auto& child : getChildren(root))
+			{
+				str.append(prefix);
+				str.append(getName(child));
+				if (auto val = getValue(child); !val.empty())
+				{
+					str.append(": ");
+					str.append(val);
+				}
+				str.push_back('\n');
+				if (hasChildren(child))
+				{
+					std::string inner_prefix = prefix;
+					inner_prefix.push_back('\t');
+					str.append(toString(child, inner_prefix));
+				}
+			}
+			return str;
+		}
 	};
 }
