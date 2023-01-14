816ca8249f3ca9400e523d40ba17f52f11f696de
Sainan <63328889+Sainan@users.noreply.github.com>
2023-01-14 19:14:04 +0100

    Add linux_so.php

---
+++ linux_so.php
@@ -0,0 +1,35 @@
+<?php
+require "linux_config.php";
+$clang .= " -D SOUP_STANDALONE";
+
+// Setup folders
+if(!is_dir("bin"))
+{
+	mkdir("bin");
+}
+if(!is_dir("bin/int"))
+{
+	mkdir("bin/int");
+}
+
+// Find work
+$files = [];
+foreach(scandir("soup") as $file)
+{
+	if(substr($file, -4) == ".cpp")
+	{
+		array_push($files, substr($file, 0, -4));
+	}
+}
+
+echo "Compiling...\n";
+$objects = [];
+foreach($files as $file)
+{
+	echo $file."\n";
+	passthru("$clang -fPIC -fvisibility=hidden -c soup/$file.cpp -o bin/int/$file.o");
+	array_push($objects, escapeshellarg("bin/int/$file.o"));
+}
+
+echo "Linking shared lib...\n";
+passthru("$clanglink --shared -o libsoup.so ".join(" ", $objects));
