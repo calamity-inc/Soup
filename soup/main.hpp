9988c7ab21114fb0e36207bb487b0e55b3145d4a
Sainan <63328889+Sainan@users.noreply.github.com>
2023-02-15 02:59:18 +0100

    Fix SOUP_MAIN_CLI for 32-bit Windows builds with non-default calling convention

--- soup/main.hpp
+++ soup/main.hpp
@@ -12,7 +12,7 @@
 #if SOUP_WINDOWS
 // make sure you have the /SUBSYTSEM linker option set to WINDOWS
 #define SOUP_MAIN_GUI(entrypoint) int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow) { return ::soup::main_impl::windows_gui(entrypoint, lpCmdLine); }
-#define SOUP_MAIN_CLI(entrypoint) int wmain(int argc, wchar_t** argv) { return ::soup::main_impl::windows(argc, argv, entrypoint, true); }
+#define SOUP_MAIN_CLI(entrypoint) int __cdecl wmain(int argc, wchar_t** argv) { return ::soup::main_impl::windows(argc, argv, entrypoint, true); }
 #else
 #define SOUP_MAIN_CLI(entrypoint) int main(int argc, const char** argv) { return ::soup::main_impl::cli(argc, argv, entrypoint); }
 #define SOUP_MAIN_GUI(entrypoint) SOUP_MAIN_CLI(entrypoint)
