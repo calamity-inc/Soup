b2e26e28dc836052002a032bf799d0182cb6c063
Sainan <63328889+Sainan@users.noreply.github.com>
2023-03-16 21:13:05 +0100

    Optimise Range::scanSSE by working through each bit of the mask instead of rescanning. Technically _mm_load_si128 would also be viable again now since we would not lose alignment, but I couldn't notice a performance difference, so I'm staying with the more "robust" one.

--- soup/Range.cpp
+++ soup/Range.cpp
@@ -82,20 +82,18 @@ namespace soup
 		auto data = sig.bytes.data();
 		auto length = sig.bytes.size();
 		const auto match = _mm_set1_epi8(data[0].value());
-		for (uintptr_t i = 0; i < size - length; )
+		for (uintptr_t i = 0; i < size - length; i += 16)
 		{
 			int mask = _mm_movemask_epi8(_mm_cmpeq_epi8(match, _mm_loadu_si128(base.add(i).as<__m128i*>())));
-			if (mask == 0)
+			while (mask != 0)
 			{
-				i += 16;
-				continue;
+				auto j = bitutil::getLeastSignificantSetBit(mask);
+				if (pattern_matches(base.add(i).add(j).add(1).as<uint8_t*>(), data + 1, length - 1))
+				{
+					return base.add(i).add(j);
+				}
+				mask &= (mask - 1); // knock out least significant set bit
 			}
-			auto j = bitutil::getLeastSignificantSetBit(mask);
-			if (pattern_matches(base.add(i).add(j).add(1).as<uint8_t*>(), data + 1, length - 1))
-			{
-				return base.add(i).add(j);
-			}
-			i += (j + 1);
 		}
 		return nullptr;
 	}
