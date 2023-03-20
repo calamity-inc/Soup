d983995449202f9d6af6ebeef74406523524f487
Sainan <sainan@calamity.gg>
2023-03-20 20:13:27 +0100

    Add visCurves

--- soup/BCanvas.cpp
+++ soup/BCanvas.cpp
@@ -4,6 +4,7 @@
 
 #include "base.hpp"
 
+#include "visCurves.hpp"
 #include "Vector2.hpp"
 
 namespace soup
@@ -29,44 +30,20 @@ namespace soup
 		}
 	}
 
-	static Vector2 quadraticBezier(const Vector2& p0, const Vector2& p1, const Vector2& p2, float t)
-	{
-		const float tinv = (1.0f - t);
-		const float t2 = (t * t);
-		const float tinv2 = (tinv * tinv);
-		return Vector2(
-			tinv2 * p0.x + 2 * tinv * t * p1.x + t2 * p2.x,
-			tinv2 * p0.y + 2 * tinv * t * p1.y + t2 * p2.y
-		);
-	}
-
 	void BCanvas::addQuadraticBezier(const Vector2& a, const Vector2& b, const Vector2& c)
 	{
 		for (float t = 0.0f; t < 1.0f; t += (0.4f / a.distance(c)))
 		{
-			auto p = quadraticBezier(a, b, c, t);
+			auto p = visCurves::quadraticBezier(a, b, c, t);
 			set(p.x, p.y);
 		}
-	}
-
-	static Vector2 cubicBezier(const Vector2& p0, const Vector2& p1, const Vector2& p2, const Vector2& p3, float t)
-	{
-		const float tinv = (1.0f - t);
-		const float t2 = (t * t);
-		const float t3 = (t2 * t);
-		const float tinv2 = (tinv * tinv);
-		const float tinv3 = (tinv2 * tinv);
-		return Vector2(
-			tinv3 * p0.x + 3 * tinv2 * t * p1.x + 3 * tinv * t2 * p2.x + t3 * p3.x,
-			tinv3 * p0.y + 3 * tinv2 * t * p1.y + 3 * tinv * t2 * p2.y + t3 * p3.y
-		);
 	}
 
 	void BCanvas::addCubicBezier(const Vector2& a, const Vector2& b, const Vector2& c, const Vector2& d)
 	{
 		for (float t = 0.0f; t < 1.0f; t += (0.4f / a.distance(d)))
 		{
-			auto p = cubicBezier(a, b, c, d, t);
+			auto p = visCurves::cubicBezier(a, b, c, d, t);
 			set(p.x, p.y);
 		}
 	}
---
+++ soup/visCurves.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "Vector2.hpp"
+
+namespace soup
+{
+	struct visCurves
+	{
+		[[nodiscard]] static Vector2 quadraticBezier(const Vector2& p0, const Vector2& p1, const Vector2& p2, float t)
+		{
+			const float tinv = (1.0f - t);
+			const float t2 = (t * t);
+			const float tinv2 = (tinv * tinv);
+			return Vector2(
+				tinv2 * p0.x + 2 * tinv * t * p1.x + t2 * p2.x,
+				tinv2 * p0.y + 2 * tinv * t * p1.y + t2 * p2.y
+			);
+		}
+
+		[[nodiscard]] static Vector2 cubicBezier(const Vector2& p0, const Vector2& p1, const Vector2& p2, const Vector2& p3, float t)
+		{
+			const float tinv = (1.0f - t);
+			const float t2 = (t * t);
+			const float t3 = (t2 * t);
+			const float tinv2 = (tinv * tinv);
+			const float tinv3 = (tinv2 * tinv);
+			return Vector2(
+				tinv3 * p0.x + 3 * tinv2 * t * p1.x + 3 * tinv * t2 * p2.x + t3 * p3.x,
+				tinv3 * p0.y + 3 * tinv2 * t * p1.y + 3 * tinv * t2 * p2.y + t3 * p3.y
+			);
+		}
+	};
+}
