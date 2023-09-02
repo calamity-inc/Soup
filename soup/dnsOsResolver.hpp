c0bfcd14b76a3a82b829f25b6a9a7ef7e1953260
Sainan <sainan@calamity.gg>
2023-09-02 13:29:17 +0200

    I have no idea what I meant by this; as far as I can tell Windows, Linux, & DoH all give identical results for the various cases I've tried

--- soup/dnsOsResolver.hpp
+++ soup/dnsOsResolver.hpp
@@ -6,7 +6,6 @@ namespace soup
 {
 	struct dnsOsResolver : public dnsResolver
 	{
-		// Note: Linux only returns records of matching type.
 		[[nodiscard]] std::vector<UniquePtr<dnsRecord>> lookup(dnsType qtype, const std::string& name) const final;
 	};
 }
