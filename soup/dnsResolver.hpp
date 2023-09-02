d19e8ebf67cad81cc99c8e599d46162c44750b0d
Sainan <sainan@calamity.gg>
2023-09-02 13:31:01 +0200

    Note that DNS lookup may result in different record types

--- soup/dnsResolver.hpp
+++ soup/dnsResolver.hpp
@@ -23,6 +23,7 @@ namespace soup
 		[[nodiscard]] std::vector<IpAddr> lookupIPv4(const std::string& name) const;
 		[[nodiscard]] std::vector<IpAddr> lookupIPv6(const std::string& name) const;
 
+		// Note that lookup may return records of differing types, e.g. (DNS_A, "au2-auto-tcp.ptoserver.com") will return CNAME record "ausd2-auto-tcp.ptoserver.com" which then gets resolved to A record "91.242.215.105", but the intermediate CNAME is still returned!
 		[[nodiscard]] virtual std::vector<UniquePtr<dnsRecord>> lookup(dnsType qtype, const std::string& name) const = 0;
 		[[nodiscard]] virtual UniquePtr<dnsLookupTask> makeLookupTask(dnsType qtype, const std::string& name) const;
 
