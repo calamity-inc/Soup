570152ea5527670201eead2b49cf5bcd2b9b2e9e
Sainan <sainan@calamity.gg>
2023-09-11 11:14:30 +0200

    Add EstablishWebSocketConnectionTask

---
+++ soup/EstablishWebSocketConnectionTask.hpp
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "base.hpp"
+#if !SOUP_WASM
+
+#include "Task.hpp"
+
+#include "netConnectTask.hpp"
+#include "Uri.hpp"
+#include "WebSocketConnection.hpp"
+
+namespace soup
+{
+	struct EstablishWebSocketConnectionTask : public Task
+	{
+		bool use_tls;
+		netConnectTask connect;
+		SharedPtr<WebSocketConnection> sock; // Output
+		std::string host;
+		std::string path;
+
+		EstablishWebSocketConnectionTask(const Uri& uri)
+			: use_tls(uri.scheme != "ws"), connect(uri.host, use_tls ? 443 : 80), host(uri.host), path(uri.path)
+		{
+		}
+
+		void onTick() final
+		{
+			if (!sock)
+			{
+				if (connect.tickUntilDone())
+				{
+					sock = connect.onDone();
+					if (use_tls)
+					{
+						sock->enableCryptoClient(host, [](Socket& s, Capture&& cap)
+						{
+							cap.get<EstablishWebSocketConnectionTask*>()->proceedToUpgrade();
+						}, this);
+					}
+					else
+					{
+						proceedToUpgrade();
+					}
+				}
+			}
+			else
+			{
+				if (sock->isWorkDoneOrClosed())
+				{
+					setWorkDone();
+				}
+			}
+		}
+
+		void proceedToUpgrade()
+		{
+			sock->upgrade(std::move(host), std::move(path), [](WebSocketConnection& con, Capture&& cap)
+			{
+				cap.get<EstablishWebSocketConnectionTask*>()->setWorkDone();
+			}, this);
+		}
+	};
+}
+
+#endif
