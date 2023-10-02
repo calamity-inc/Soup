581bebc0bc2304a216676c06dd4aa387bf6d8140
Sainan <sainan@calamity.gg>
2023-10-02 04:17:15 +0200

    Make visKeyboard::Keys canon

--- soup/visKeyboard.hpp
+++ soup/visKeyboard.hpp
@@ -3,6 +3,8 @@
 #include <cstring> // memset
 
 #include "fwd.hpp"
+
+#include "keys.hpp"
 #include "scancodes.hpp"
 
 namespace soup
@@ -10,92 +12,6 @@ namespace soup
 	class visKeyboard
 	{
 	public:
-		enum Keys : uint8_t
-		{
-			KEY_BACKQUOTE = 0,
-			KEY_1,
-			KEY_2,
-			KEY_3,
-			KEY_4,
-			KEY_5,
-			KEY_6,
-			KEY_7,
-			KEY_8,
-			KEY_9,
-			KEY_0,
-			KEY_MINUS,
-			KEY_EQUALS,
-			KEY_BACKSPACE,
-			KEY_TAB,
-			KEY_Q,
-			KEY_W,
-			KEY_E,
-			KEY_R,
-			KEY_T,
-			KEY_Y,
-			KEY_U,
-			KEY_I,
-			KEY_O,
-			KEY_P,
-			KEY_BRACKET_LEFT,
-			KEY_BRACKET_RIGHT,
-			KEY_ENTER,
-			KEY_CAPS_LOCK,
-			KEY_A,
-			KEY_S,
-			KEY_D,
-			KEY_F,
-			KEY_G,
-			KEY_H,
-			KEY_J,
-			KEY_K,
-			KEY_L,
-			KEY_SEMICOLON,
-			KEY_QUOTE,
-			KEY_BACKSLASH,
-			KEY_LSHIFT,
-			KEY_INTL_BACKSLASH,
-			KEY_Z,
-			KEY_X,
-			KEY_C,
-			KEY_V,
-			KEY_B,
-			KEY_N,
-			KEY_M,
-			KEY_COMMA,
-			KEY_PERIOD,
-			KEY_SLASH,
-			KEY_RSHIFT,
-			KEY_LCTRL,
-			KEY_LMETA,
-			KEY_LALT,
-			KEY_SPACE,
-			KEY_RALT,
-			KEY_RMETA,
-			KEY_FN,
-			KEY_RCTRL,
-
-			KEY_NUM_LOCK,
-			KEY_NUMPAD_DIVIDE,
-			KEY_NUMPAD_MULTIPLY,
-			KEY_NUMPAD_SUBTRACT,
-			KEY_NUMPAD7,
-			KEY_NUMPAD8,
-			KEY_NUMPAD9,
-			KEY_NUMPAD_ADD,
-			KEY_NUMPAD4,
-			KEY_NUMPAD5,
-			KEY_NUMPAD6,
-			KEY_NUMPAD1,
-			KEY_NUMPAD2,
-			KEY_NUMPAD3,
-			KEY_NUMPAD_ENTER,
-			KEY_NUMPAD0,
-			KEY_NUMPAD_DECIMAL,
-
-			NUM_KEYS
-		};
-
 		uint8_t values[NUM_KEYS];
 
 		visKeyboard() noexcept
