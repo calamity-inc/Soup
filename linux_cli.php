9e6f50bb950c63d1ef2418716867f0363f7d1bc0
Sainan <sainan@calamity.gg>
2023-10-15 04:39:41 +0200

    Ensure CLI executable is called "soup" everywhere for consistency

--- linux_cli.php
+++ linux_cli.php
@@ -1,4 +1,4 @@
 <?php
 require "linux_config.php";
 
-passthru("$clanglink \"CLI/cli.cpp\" \"CLI/cli_3d.cpp\" \"CLI/cli_chatgpt.cpp\" \"CLI/cli_dig.cpp\" \"CLI/cli_datareflection.cpp\" \"CLI/cli_dnsserver.cpp\" \"CLI/cli_dvd.cpp\" \"CLI/cli_ircserver.cpp\" \"CLI/cli_keyboard.cpp\" \"CLI/cli_maze.cpp\" \"CLI/cli_morse.cpp\" \"CLI/cli_repl.cpp\" \"CLI/cli_snake.cpp\" \"CLI/cli_test.cpp\" \"CLI/cli_websrv.cpp\" libsoup.a -I soup -o \"soupcli\"");
+passthru("$clanglink \"CLI/cli.cpp\" \"CLI/cli_3d.cpp\" \"CLI/cli_chatgpt.cpp\" \"CLI/cli_dig.cpp\" \"CLI/cli_datareflection.cpp\" \"CLI/cli_dnsserver.cpp\" \"CLI/cli_dvd.cpp\" \"CLI/cli_ircserver.cpp\" \"CLI/cli_keyboard.cpp\" \"CLI/cli_maze.cpp\" \"CLI/cli_morse.cpp\" \"CLI/cli_repl.cpp\" \"CLI/cli_snake.cpp\" \"CLI/cli_test.cpp\" \"CLI/cli_websrv.cpp\" libsoup.a -I soup -o \"CLI/soup\"");
