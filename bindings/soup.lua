274d315fac7e68879996183305558a4d67782431
Sainan <sainan@calamity.gg>
2023-11-26 08:08:07 +0100

    Remove link to Soup-Lua-Bindings

--- bindings/soup.lua
+++ bindings/soup.lua
@@ -1,6 +1,3 @@
--- This is an example of using an FFI library to interface with Soup's C API.
--- However, there is a more native way of using Soup via Lua: https://github.com/calamity-inc/Soup-Lua-Bindings
-
 local ffi = require("luaffi") -- https://github.com/calamity-inc/luaffi
 local soup = ffi.open("soup")
 
