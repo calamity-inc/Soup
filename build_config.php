097f93bd413a2958b5c35801abe03b5b86c11dce
Sainan <sainan@calamity.gg>
2023-12-05 13:54:46 +0100

    Don't set -lstdc++fs on MacOS

--- build_config.php
+++ build_config.php
@@ -1,7 +1,7 @@
 <?php
 $clang = "clang -std=c++17 -fno-rtti";
-$clanglink = $clang." -lstdc++ -lstdc++fs -pthread -lresolv -lm -ldl";
+$clanglink = $clang." -lstdc++ -pthread -lresolv -lm -ldl";
 if (PHP_OS_FAMILY != "Darwin")
 {
-	$clanglink .= " -fuse-ld=lld";
+	$clanglink .= " -fuse-ld=lld -lstdc++fs";
 }
