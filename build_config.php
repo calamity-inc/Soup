9d3d7769e45d6db979cd8b27644ce65dcf5c6938
Sainan <sainan@calamity.gg>
2023-12-05 13:39:13 +0100

    Don't set -fuse-ld=lld on MacOS

--- build_config.php
+++ build_config.php
@@ -1,3 +1,7 @@
 <?php
 $clang = "clang -std=c++17 -fno-rtti";
-$clanglink = $clang." -fuse-ld=lld -lstdc++ -lstdc++fs -pthread -lresolv -lm -ldl";
+$clanglink = $clang." -lstdc++ -lstdc++fs -pthread -lresolv -lm -ldl";
+if (PHP_OS_FAMILY != "Darwin")
+{
+	$clanglink .= " -fuse-ld=lld";
+}
