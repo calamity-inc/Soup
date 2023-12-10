22e507b28ea166f3a5696f8579874b1b21fe34a9
Sainan <sainan@calamity.gg>
2023-12-10 11:36:39 +0100

    PHP build scripts: Define _CRT_SECURE_NO_WARNINGS on Windows

--- build_config.php
+++ build_config.php
@@ -1,5 +1,10 @@
 <?php
 $clang = "clang -std=c++17 -fno-rtti";
+if (defined("PHP_WINDOWS_VERSION_MAJOR"))
+{
+	$clang .= " -D_CRT_SECURE_NO_WARNINGS";
+}
+
 $clanglink = $clang." -lstdc++ -pthread -lresolv -lm -ldl";
 if (PHP_OS_FAMILY != "Darwin")
 {
