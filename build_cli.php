54d1ed0c721d137e425c5e45d7ecca7ab33da5d8
Sainan <sainan@calamity.gg>
2024-01-27 13:27:10 +0100

    Fix build_cli.php for Windows

--- build_cli.php
+++ build_cli.php
@@ -1,4 +1,13 @@
 <?php
 require "build_common.php";
 
-passthru("$clanglink \"CLI/cli.cpp\" \"CLI/cli_3d.cpp\" \"CLI/cli_chatgpt.cpp\" \"CLI/cli_dig.cpp\" \"CLI/cli_datareflection.cpp\" \"CLI/cli_dnsserver.cpp\" \"CLI/cli_dvd.cpp\" \"CLI/cli_ircserver.cpp\" \"CLI/cli_keyboard.cpp\" \"CLI/cli_maze.cpp\" \"CLI/cli_mesh.cpp\" \"CLI/cli_midi.cpp\" \"CLI/cli_morse.cpp\" \"CLI/cli_repl.cpp\" \"CLI/cli_snake.cpp\" \"CLI/cli_test.cpp\" \"CLI/cli_websrv.cpp\" libsoup.a -I soup -o \"CLI/soup\"");
+$cmd = "$clanglink \"CLI/cli.cpp\" \"CLI/cli_3d.cpp\" \"CLI/cli_chatgpt.cpp\" \"CLI/cli_dig.cpp\" \"CLI/cli_datareflection.cpp\" \"CLI/cli_dnsserver.cpp\" \"CLI/cli_dvd.cpp\" \"CLI/cli_ircserver.cpp\" \"CLI/cli_keyboard.cpp\" \"CLI/cli_maze.cpp\" \"CLI/cli_mesh.cpp\" \"CLI/cli_midi.cpp\" \"CLI/cli_morse.cpp\" \"CLI/cli_repl.cpp\" \"CLI/cli_snake.cpp\" \"CLI/cli_test.cpp\" \"CLI/cli_websrv.cpp\" ";
+if (defined("PHP_WINDOWS_VERSION_MAJOR"))
+{
+	$cmd .= "soup.lib -I soup -o \"CLI/soup.exe\"";
+}
+else
+{
+	$cmd .= "libsoup.a -I soup -o \"CLI/soup\"";
+}
+passthru($cmd);
