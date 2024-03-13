7e33a9f1e21a30ae04daed2645b4ed83f864e667
Sainan <sainan@calamity.gg>
2024-03-13 04:18:16 +0100

    Remove deprecated netAs::isHosting overload

--- soup/netAs.hpp
+++ soup/netAs.hpp
@@ -8,9 +8,8 @@
 
 namespace soup
 {
-	class netAs
+	struct netAs
 	{
-	public:
 		uint32_t number;
 		const char* handle;
 		const char* name;
@@ -24,8 +23,5 @@ namespace soup
 
 		// Does the AS belong to a hosting provider? Can be used to tell if this is a VPN.
 		[[nodiscard]] bool isHosting(const netIntel& intel) const SOUP_EXCAL;
-		[[deprecated("Provide your netIntel instance as an argument for more up-to-date results.")]] bool isHosting() const SOUP_EXCAL;
-	private:
-		[[nodiscard]] bool isHosting(const std::string& extra_wasm) const SOUP_EXCAL;
 	};
 }
