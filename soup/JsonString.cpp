970786ed008b8d37ac8de00f4aacad8f4eea2f77
Sainan <sainan@calamity.gg>
2024-06-01 18:08:17 +0200

    Optimise JsonString::encodeAndAppendTo

--- soup/JsonString.cpp
+++ soup/JsonString.cpp
@@ -107,14 +107,34 @@ NAMESPACE_SOUP
 
 	void JsonString::encodeAndAppendTo(std::string& str) const SOUP_EXCAL
 	{
-		std::string encoded = *this;
-		string::replaceAll(encoded, "\\", "\\\\");
-		string::replaceAll(encoded, "\"", "\\\"");
-		string::replaceAll(encoded, "\r", "\\r");
-		string::replaceAll(encoded, "\n", "\\n");
-		encoded.insert(0, 1, '"');
-		encoded.push_back('"');
-		str.append(encoded);
+		str.reserve(str.size() + value.size() + 2);
+		str.push_back('"');
+		for (const auto& c : value)
+		{
+			switch (c)
+			{
+			default:
+				str.push_back(c);
+				break;
+
+			case '\\':
+				str.append("\\\\");
+				break;
+
+			case '\"':
+				str.append("\\\"");
+				break;
+
+			case '\r':
+				str.append("\\r");
+				break;
+
+			case '\n':
+				str.append("\\n");
+				break;
+			}
+		}
+		str.push_back('"');
 	}
 
 	bool JsonString::binaryEncode(Writer& w) const
