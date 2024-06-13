6fc68c791eddf1f07b29f5f9d57de936bf58a733
Sainan <sainan@calamity.gg>
2024-06-13 12:24:53 +0200

    Add MimeMessage::getBody as a non-mutating alternative to decode

--- soup/MimeMessage.cpp
+++ soup/MimeMessage.cpp
@@ -24,6 +24,21 @@ NAMESPACE_SOUP
 		setContentLength();
 	}
 
+	std::string MimeMessage::getBody() const
+	{
+		if (auto enc = header_fields.find(ObfusString("Content-Encoding")); enc != header_fields.end())
+		{
+			auto enc_joaat = joaat::hash(enc->second);
+			switch (enc_joaat)
+			{
+			case joaat::hash("gzip"):
+			case joaat::hash("deflate"):
+				return deflate::decompress(body).decompressed;
+			}
+		}
+		return body;
+	}
+
 	void MimeMessage::setContentLength()
 	{
 		header_fields.emplace(ObfusString("Content-Length"), std::to_string(body.size()));
