8cf5cfdc9b9695c241f5fc1d8a3b8ab3533d272d
Sainan <sainan@calamity.gg>
2024-06-19 19:32:34 +0200

    Fix 32-bit compile with MSVC

--- Intrin/crc32c_intrin.cpp
+++ Intrin/crc32c_intrin.cpp
@@ -9,12 +9,21 @@ namespace soup_intrin
 	uint32_t crc32c_intrin(const uint8_t* data, size_t size, uint32_t initial) noexcept
 	{
 		uint32_t i = ~initial;
+#if defined(__x86_64__) || defined(_M_X64)
 		while (size >= 8)
 		{
 			i = (uint32_t)_mm_crc32_u64(i, *reinterpret_cast<const uint64_t*>(data));
 			data += 8;
 			size -= 8;
 		}
+#else
+		while (size >= 4)
+		{
+			i = _mm_crc32_u32(i, *reinterpret_cast<const uint32_t*>(data));
+			data += 4;
+			size -= 4;
+		}
+#endif
 		while (size > 0)
 		{
 			i = _mm_crc32_u8(i, *data);
