0badf8f71accda3abe260ad8231e361710718e26
Sainan <sainan@calamity.gg>
2024-07-04 03:56:38 +0200

    Add bitutil::reverse

--- soup/bitutil.hpp
+++ soup/bitutil.hpp
@@ -121,6 +121,15 @@ NAMESPACE_SOUP
 #endif
 		}
 
+		// https://stackoverflow.com/a/2602885
+		[[nodiscard]] static uint8_t reverse(uint8_t b) noexcept
+		{
+			b = (b & 0xF0) >> 4 | (b & 0x0F) << 4;
+			b = (b & 0xCC) >> 2 | (b & 0x33) << 2;
+			b = (b & 0xAA) >> 1 | (b & 0x55) << 1;
+			return b;
+		}
+
 		[[nodiscard]] static std::vector<bool> interleave(const std::vector<std::vector<bool>>& data); // assumes that all inner vectors have the same size
 	};
 }
