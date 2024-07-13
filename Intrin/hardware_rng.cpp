11f85fbfe7ebd5b748aa527d2176b10cfdd58bab
Sainan <sainan@calamity.gg>
2024-07-13 02:13:41 +0200

    Improve FastHardwareRng to always be available

--- Intrin/hardware_rng.cpp
+++ Intrin/hardware_rng.cpp
@@ -6,8 +6,6 @@
 
 namespace soup_intrin
 {
-	// RDSEED
-
 	uint16_t hardware_rng_generate16() noexcept
 	{
 		uint16_t res;
@@ -27,32 +25,6 @@ namespace soup_intrin
 	{
 		unsigned long long res;
 		while (_rdseed64_step(&res) == 0);
-		return res;
-	}
-	static_assert(sizeof(uint64_t) == sizeof(unsigned long long));
-#endif
-
-	// RDRAND
-
-	uint16_t fast_hardware_rng_generate16() noexcept
-	{
-		uint16_t res;
-		while (_rdrand16_step(&res) == 0);
-		return res;
-	}
-
-	uint32_t fast_hardware_rng_generate32() noexcept
-	{
-		uint32_t res;
-		while (_rdrand32_step(&res) == 0);
-		return res;
-	}
-
-#if defined(__x86_64__) || defined(_M_X64)
-	uint64_t fast_hardware_rng_generate64() noexcept
-	{
-		unsigned long long res;
-		while (_rdrand64_step(&res) == 0);
 		return res;
 	}
 	static_assert(sizeof(uint64_t) == sizeof(unsigned long long));
