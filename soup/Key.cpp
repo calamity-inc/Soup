0bd128f87f3f0f83573b84ba87bd2ca99d4a74dc
Sainan <63328889+Sainan@users.noreply.github.com>
2024-08-04 13:51:39 +0200

    Fix soup_key_to_virtual_key & soup_key_to_translated_virtual_key MapVirtualKeyA seems to use the active layout, so the call to MapVirtualKeyExA was overkill.

--- soup/Key.cpp
+++ soup/Key.cpp
@@ -3,13 +3,140 @@
 #include "Ps2Scancode.hpp"
 
 #if SOUP_WINDOWS
-#include "Window.hpp"
+#include <windows.h>
 #endif
 
 NAMESPACE_SOUP
 {
 #if SOUP_WINDOWS
-	[[nodiscard]] static int soup_key_to_virtual_key_common(uint8_t key)
+	int soup_key_to_virtual_key(uint8_t key) noexcept
+	{
+		switch (key)
+		{
+		case KEY_ESCAPE: return VK_ESCAPE;
+		case KEY_F1: return VK_F1;
+		case KEY_F2: return VK_F2;
+		case KEY_F3: return VK_F3;
+		case KEY_F4: return VK_F4;
+		case KEY_F5: return VK_F5;
+		case KEY_F6: return VK_F6;
+		case KEY_F7: return VK_F7;
+		case KEY_F8: return VK_F8;
+		case KEY_F9: return VK_F9;
+		case KEY_F10: return VK_F10;
+		case KEY_F11: return VK_F11;
+		case KEY_F12: return VK_F12;
+
+		case KEY_BACKQUOTE: return VK_OEM_3;
+		case KEY_1: return '1';
+		case KEY_2: return '2';
+		case KEY_3: return '3';
+		case KEY_4: return '4';
+		case KEY_5: return '5';
+		case KEY_6: return '6';
+		case KEY_7: return '7';
+		case KEY_8: return '8';
+		case KEY_9: return '9';
+		case KEY_0: return '0';
+		case KEY_MINUS: return VK_OEM_MINUS;
+		case KEY_EQUALS: return VK_OEM_PLUS;
+		case KEY_BACKSPACE: return VK_BACK;
+		case KEY_TAB: return VK_TAB;
+		case KEY_Q: return 'Q';
+		case KEY_W: return 'W';
+		case KEY_E: return 'E';
+		case KEY_R: return 'R';
+		case KEY_T: return 'T';
+		case KEY_Y: return 'Y';
+		case KEY_U: return 'U';
+		case KEY_I: return 'I';
+		case KEY_O: return 'O';
+		case KEY_P: return 'P';
+		case KEY_BRACKET_LEFT: return VK_OEM_4;
+		case KEY_BRACKET_RIGHT: return VK_OEM_6;
+		case KEY_ENTER: return VK_RETURN;
+		case KEY_CAPS_LOCK: return VK_CAPITAL;
+		case KEY_A: return 'A';
+		case KEY_S: return 'S';
+		case KEY_D: return 'D';
+		case KEY_F: return 'F';
+		case KEY_G: return 'G';
+		case KEY_H: return 'H';
+		case KEY_J: return 'J';
+		case KEY_K: return 'K';
+		case KEY_L: return 'L';
+		case KEY_SEMICOLON: return VK_OEM_1;
+		case KEY_QUOTE: return VK_OEM_7;
+		case KEY_BACKSLASH: return VK_OEM_5;
+		case KEY_LSHIFT: return VK_LSHIFT;
+		case KEY_INTL_BACKSLASH: return VK_OEM_102;
+		case KEY_Z: return 'Z';
+		case KEY_X: return 'X';
+		case KEY_C: return 'C';
+		case KEY_V: return 'V';
+		case KEY_B: return 'B';
+		case KEY_N: return 'N';
+		case KEY_M: return 'M';
+		case KEY_COMMA: return VK_OEM_COMMA;
+		case KEY_PERIOD: return VK_OEM_PERIOD;
+		case KEY_SLASH: return VK_OEM_2;
+		case KEY_RSHIFT: return VK_RSHIFT;
+		case KEY_LCTRL: return VK_LCONTROL;
+		case KEY_LMETA: return VK_LWIN;
+		case KEY_LALT: return VK_LMENU;
+		case KEY_SPACE: return VK_SPACE;
+		case KEY_RALT: return VK_RMENU;
+		case KEY_RMETA: return VK_RWIN;
+		//case KEY_FN: return VK_FN;
+		case KEY_RCTRL: return VK_RCONTROL;
+
+		case KEY_PRINT_SCREEN: return VK_SNAPSHOT;
+		case KEY_PAUSE: return VK_PAUSE;
+		case KEY_SCROLL_LOCK: return VK_SCROLL;
+
+		case KEY_INSERT: return VK_INSERT;
+		case KEY_HOME: return VK_HOME;
+		case KEY_PAGE_UP: return VK_PRIOR;
+		case KEY_DEL: return VK_DELETE;
+		case KEY_END: return VK_END;
+		case KEY_PAGE_DOWN: return VK_NEXT;
+
+		case KEY_ARROW_UP: return VK_UP;
+		case KEY_ARROW_LEFT: return VK_LEFT;
+		case KEY_ARROW_DOWN: return VK_DOWN;
+		case KEY_ARROW_RIGHT: return VK_RIGHT;
+
+		case KEY_NUM_LOCK: return VK_NUMLOCK;
+		case KEY_NUMPAD_DIVIDE: return VK_DIVIDE;
+		case KEY_NUMPAD_MULTIPLY: return VK_MULTIPLY;
+		case KEY_NUMPAD_SUBTRACT: return VK_SUBTRACT;
+		case KEY_NUMPAD7: return VK_NUMPAD7;
+		case KEY_NUMPAD8: return VK_NUMPAD8;
+		case KEY_NUMPAD9: return VK_NUMPAD9;
+		case KEY_NUMPAD_ADD: return VK_ADD;
+		case KEY_NUMPAD4: return VK_NUMPAD4;
+		case KEY_NUMPAD5: return VK_NUMPAD5;
+		case KEY_NUMPAD6: return VK_NUMPAD6;
+		case KEY_NUMPAD1: return VK_NUMPAD1;
+		case KEY_NUMPAD2: return VK_NUMPAD2;
+		case KEY_NUMPAD3: return VK_NUMPAD3;
+		case KEY_NUMPAD_ENTER: return VK_RETURN; // There were plans to allocate VK_NUMPAD_RETURN as 0x0E, but for now this is not the case.
+		case KEY_NUMPAD0: return VK_NUMPAD0;
+		case KEY_NUMPAD_DECIMAL: return VK_DECIMAL;
+
+		case KEY_STOP_MEDIA: return VK_MEDIA_STOP;
+		case KEY_PREV_TRACK: return VK_MEDIA_PREV_TRACK;
+		case KEY_PLAY_PAUSE: return VK_MEDIA_PLAY_PAUSE;
+		case KEY_NEXT_TRACK: return VK_MEDIA_NEXT_TRACK;
+
+		case KEY_F13: return VK_F13;
+		case KEY_F14: return VK_F14;
+		case KEY_F15: return VK_F15;
+		}
+		return 0;
+	}
+
+	int soup_key_to_translated_virtual_key(uint8_t key) noexcept
 	{
 		// MapVirtualKeyA would return VK_INSERT instead of VK_NUMPAD0 etc.
 		switch (key)
@@ -27,26 +154,7 @@ NAMESPACE_SOUP
 		case KEY_NUMPAD_DECIMAL: return VK_DECIMAL;
 		default:;
 		}
-		return 0;
-	}
-
-	int soup_key_to_virtual_key(uint8_t key) noexcept
-	{
-		if (int vk = soup_key_to_virtual_key_common(key))
-		{
-			return vk;
-		}
 		return MapVirtualKeyA(soup_key_to_ps2_scancode(key), MAPVK_VSC_TO_VK_EX);
-	}
-
-	int soup_key_to_translated_virtual_key(uint8_t key) noexcept
-	{
-		if (int vk = soup_key_to_virtual_key_common(key))
-		{
-			return vk;
-		}
-		const auto layout = GetKeyboardLayout(Window::getFocused().getOwnerTid());
-		return MapVirtualKeyExA(soup_key_to_ps2_scancode(key), MAPVK_VSC_TO_VK_EX, layout);
 	}
 
 	Key virtual_key_to_soup_key(int vk) noexcept
