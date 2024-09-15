28374c82d000d0c655cb330df35ca543c1cc9871
Sainan <sainan@calamity.inc>
2024-09-15 15:28:37 +0200

    Add MaintainWebSocketConnectionTask

---
+++ soup/MaintainWebSocketConnectionTask.cpp
@@ -0,0 +1,44 @@
+#include "MaintainWebSocketConnectionTask.hpp"
+#if !SOUP_WASM
+
+#include "time.hpp"
+#include "WebSocketFrameType.hpp"
+
+#define HEARTBEAT_INTERVAL_MS (5 * 60 * 1000)
+
+NAMESPACE_SOUP
+{
+	MaintainWebSocketConnectionTask::MaintainWebSocketConnectionTask(SharedPtr<WebSocketConnection> sock, on_frame_t on_frame)
+		: sock(std::move(sock)), on_frame(on_frame), next_heartbeat(time::millis() + HEARTBEAT_INTERVAL_MS)
+	{
+		recvLoop(*this->sock);
+	}
+
+	void MaintainWebSocketConnectionTask::onTick()
+	{
+		SOUP_IF_UNLIKELY (sock->isWorkDoneOrClosed())
+		{
+			setWorkDone();
+			return;
+		}
+		SOUP_IF_UNLIKELY (time::millis() >= next_heartbeat)
+		{
+			sock->wsSend(WebSocketFrameType::PING, {});
+			next_heartbeat = time::millis() + HEARTBEAT_INTERVAL_MS;
+		}
+	}
+
+	void MaintainWebSocketConnectionTask::recvLoop(WebSocketConnection& s)
+	{
+		s.wsRecv([](WebSocketConnection& s, WebSocketMessage&& msg, Capture&& cap) SOUP_EXCAL
+		{
+			auto task = cap.get<MaintainWebSocketConnectionTask*>();
+			if (!msg.data.empty())
+			{
+				task->on_frame(s, std::move(msg));
+			}
+			task->recvLoop(s);
+		}, this);
+	}
+}
+#endif
---
+++ soup/MaintainWebSocketConnectionTask.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "Task.hpp"
+#if !SOUP_WASM
+
+#include <ctime> // time_t
+
+#include "SharedPtr.hpp"
+#include "WebSocketConnection.hpp"
+
+NAMESPACE_SOUP
+{
+	class MaintainWebSocketConnectionTask : public Task
+	{
+	protected:
+		using on_frame_t = void(*)(WebSocketConnection&, WebSocketMessage&&) SOUP_EXCAL;
+
+		SharedPtr<WebSocketConnection> sock;
+		on_frame_t on_frame;
+		time_t next_heartbeat;
+
+	public:
+		MaintainWebSocketConnectionTask(SharedPtr<WebSocketConnection> sock, on_frame_t on_frame);
+
+		void onTick() final;
+
+	protected:
+		void recvLoop(WebSocketConnection& s);
+	};
+}
+
+#endif
