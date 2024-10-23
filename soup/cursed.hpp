eb431e609b3ffa022e9d7c6c6ab516ac2a7d9787
Sainan <sainan@calamity.inc>
2024-10-23 20:05:34 +0200

    Add some more cursed stuff

--- soup/cursed.hpp
+++ soup/cursed.hpp
@@ -20,4 +20,44 @@ NAMESPACE_SOUP
 		static_assert(sizeof(Desired) == sizeof(Actual));
 		return ub_cast<Desired, Actual>(a);
 	}
+
+
+	template <typename StructT, typename FieldT>
+	uintptr_t offsetofp(FieldT StructT::* ptr)
+	{
+		return reinterpret_cast<uintptr_t>(&(ub_pointer_cast<StructT*>(nullptr)->*ptr));
+	}
+
+
+	template<typename Tag>
+	struct PrivateMemberResult
+	{
+		typedef typename Tag::type type;
+		static type ptr;
+	};
+
+	template<typename Tag>
+	typename PrivateMemberResult<Tag>::type PrivateMemberResult<Tag>::ptr;
+
+	template<typename Tag, typename Tag::type p>
+	struct PrivateMemberGetter : PrivateMemberResult<Tag>
+	{
+		struct PrivateMemberFiller
+		{
+			PrivateMemberFiller()
+			{
+				PrivateMemberResult<Tag>::ptr = p;
+			}
+		};
+		static PrivateMemberFiller filler_obj;
+	};
+
+	template<typename Tag, typename Tag::type p>
+	typename PrivateMemberGetter<Tag, p>::PrivateMemberFiller PrivateMemberGetter<Tag, p>::filler_obj;
+
+	#define SOUP_PREPARE_MEMBER(StructT, MemberT, member_name) \
+	struct PrivateMemberTag_ ## StructT ## _ ## member_name { typedef MemberT StructT::* type; }; \
+	NAMESPACE_SOUP { template struct PrivateMemberGetter<PrivateMemberTag_ ## StructT ## _ ## member_name, &StructT::member_name>; }
+
+	#define SOUP_GET_MEMBER(StructT, member_name) (PrivateMemberResult<PrivateMemberTag_ ## StructT ## _ ## member_name>::ptr)
 }
