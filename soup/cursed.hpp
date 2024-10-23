e3ac67153d467ec739aa411168018bed71bafe82
Sainan <sainan@calamity.inc>
2024-10-23 20:05:25 +0200

    Improve ub_cast & ub_pointer_cast

--- soup/cursed.hpp
+++ soup/cursed.hpp
@@ -5,23 +5,19 @@
 NAMESPACE_SOUP
 {
 	template <typename Desired, typename Actual>
-	[[nodiscard]] Desired ub_pointer_cast(Actual a)
-	{
-		static_assert(sizeof(Desired) == sizeof(Actual));
-		union {
-			Actual a;
-			Desired d;
-		} u{ a = a };
-		return u.d;
-	}
-
-	template <typename Desired, typename Actual>
 	[[nodiscard]] Desired ub_cast(Actual a)
 	{
 		union {
 			Actual a;
 			Desired d;
 		} u{ a = a };
-		return u.d;
+		SOUP_MOVE_RETURN(u.d);
+	}
+
+	template <typename Desired, typename Actual>
+	[[nodiscard]] Desired ub_pointer_cast(Actual a)
+	{
+		static_assert(sizeof(Desired) == sizeof(Actual));
+		return ub_cast<Desired, Actual>(a);
 	}
 }
