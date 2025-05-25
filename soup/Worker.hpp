b24d79f478909db5db15d8d4d5739e4a5f0a7343
Sainan <63328889+Sainan@users.noreply.github.com>
2025-05-25 09:00:25 +0200

    Add Worker::flags

--- soup/Worker.hpp
+++ soup/Worker.hpp
@@ -16,7 +16,13 @@ NAMESPACE_SOUP
 		WORKER_TYPE_UNSPECIFIED = 0,
 		WORKER_TYPE_SOCKET,
 		WORKER_TYPE_TASK,
-		WORKER_TYPE_USER,
+		WORKER_TYPE_USER // use this as the counting base for your own types
+	};
+
+	enum : uint8_t
+	{
+		WORKER_FLAG_USER, // use this as the counting base for your own flags
+		WORKER_FLAG_SIZE = 8
 	};
 
 	struct Worker
@@ -33,6 +39,7 @@ NAMESPACE_SOUP
 		uint8_t type;
 		uint8_t recursions = 0;
 		HoldupType holdup_type = NONE;
+		uint8_t flags = 0;
 		Callback<void(Worker&)> holdup_callback;
 		void* holdup_data;
 
