ce542f78b06cb4a82092855b8352b16bccfe8752
Sainan <63328889+Sainan@users.noreply.github.com>
2025-05-27 12:38:45 +0200

    Deprecate silly BitReader/BitWriter functions

--- soup/BitReader.hpp
+++ soup/BitReader.hpp
@@ -76,13 +76,13 @@ NAMESPACE_SOUP
 
 		bool b(bool& out);
 		bool u8(uint8_t bits, uint8_t& out);
-		bool u16_dyn_2(uint16_t& val); // A 16-bit value encoded using 5-18 bits. Assumes that smaller numbers are the norm.
-		bool u17_dyn_2(uint32_t& val); // A 17-bit value encoded using 5-19 bits. Assumes that smaller numbers are the norm.
-		bool u20_dyn(uint32_t& val); // A 20-bit value encoded using 6-22 bits. Assumes that smaller numbers are the norm.
-		bool u32_dyn(uint32_t& val); // A 32-bit value encoded using 10-34 bits. Assumes that smaller numbers are the norm.
-		bool str_utf8_nt(std::string& str); // A UTF-8 string encoded using 6-23 bits per codepoint. Null-terminated.
-		bool str_utf32_nt(std::u32string& str); // A UTF-32 string encoded using 6-23 bits per codepoint. Null-terminated.
-		bool str_utf8_lp(std::string& str, uint8_t lpbits); // A UTF-8 string encoded using 6-23 bits per codepoint. Length-prefixed.
-		bool str_utf32_lp(std::u32string& str, uint8_t lpbits); // A UTF-32 string encoded using 6-23 bits per codepoint. Length-prefixed.
+		[[deprecated]] bool u16_dyn_2(uint16_t& val); // A 16-bit value encoded using 5-18 bits. Assumes that smaller numbers are the norm.
+		[[deprecated]] bool u17_dyn_2(uint32_t& val); // A 17-bit value encoded using 5-19 bits. Assumes that smaller numbers are the norm.
+		/*[[deprecated]]*/ bool u20_dyn(uint32_t& val); // A 20-bit value encoded using 6-22 bits. Assumes that smaller numbers are the norm.
+		[[deprecated]] bool u32_dyn(uint32_t& val); // A 32-bit value encoded using 10-34 bits. Assumes that smaller numbers are the norm.
+		/*[[deprecated]]*/ bool str_utf8_nt(std::string& str); // A UTF-8 string encoded using 6-23 bits per codepoint. Null-terminated.
+		/*[[deprecated]]*/ bool str_utf32_nt(std::u32string& str); // A UTF-32 string encoded using 6-23 bits per codepoint. Null-terminated.
+		[[deprecated]] bool str_utf8_lp(std::string& str, uint8_t lpbits); // A UTF-8 string encoded using 6-23 bits per codepoint. Length-prefixed.
+		[[deprecated]] bool str_utf32_lp(std::u32string& str, uint8_t lpbits); // A UTF-32 string encoded using 6-23 bits per codepoint. Length-prefixed.
 	};
 }
