d860112e35b1e7c9918bb3fffb49f22972239cc8
Sainan <63328889+Sainan@users.noreply.github.com>
2025-06-04 15:57:58 +0200

    Make osRegistry header-only This should help dead code elimination

--- soup/osRegistry.hpp
+++ soup/osRegistry.hpp
@@ -1,8 +1,16 @@
 #pragma once
 
+#include "base.hpp"
+#if SOUP_WINDOWS
+
 #include <string>
 
 #include <windows.h>
+
+#include "Exception.hpp"
+#include "unicode.hpp"
+
+#pragma comment(lib, "Advapi32")
 
 NAMESPACE_SOUP
 {
@@ -25,16 +33,62 @@ NAMESPACE_SOUP
 				return (((LONG)(ULONG_PTR)h) & ((LONG)0x80000000)) != 0;
 			}
 
-			[[nodiscard]] bool hasSubkey(const char* name) const noexcept;
-			[[nodiscard]] Key getSubkey(const char* name) const;
-			[[nodiscard]] Key createSubkey(const char* name) const;
+			[[nodiscard]] bool hasSubkey(const char* name) const noexcept
+			{
+				HKEY k;
+				if (RegOpenKeyA(h, name, &k) == ERROR_SUCCESS)
+				{
+					RegCloseKey(k);
+					return true;
+				}
+				return false;
+			}
 
-			void setValue(const std::string& value) const;
-			void setValue(const char* key, const std::string& value) const;
+			[[nodiscard]] Key getSubkey(const char* name) const
+			{
+				HKEY k;
+				if (RegOpenKeyA(h, name, &k) == ERROR_SUCCESS)
+				{
+					return Key{ k };
+				}
+				SOUP_THROW(Exception("Failed to open registry key"));
+			}
+
+			[[nodiscard]] Key createSubkey(const char* name) const
+			{
+				HKEY k;
+				if (RegCreateKeyExA(h, name, 0, nullptr, REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS, nullptr, &k, nullptr) == ERROR_SUCCESS)
+				{
+					return Key{ k };
+				}
+				SOUP_THROW(Exception("Failed to create registry key"));
+			}
+
+			void setValue(const std::string& value) const
+			{
+				setValue(nullptr, value);
+			}
+
+			void setValue(const char* key, const std::string& value) const
+			{
+				auto value_utf16 = unicode::utf8_to_utf16(value);
+
+				if (key == nullptr)
+				{
+					RegSetValueExW(h, nullptr, 0, REG_SZ, (const BYTE*)value_utf16.data(), static_cast<DWORD>(value_utf16.size() * sizeof(UTF16_CHAR_TYPE)));
+				}
+				else
+				{
+					auto key_utf16 = unicode::utf8_to_utf16(key);
+					RegSetValueExW(h, key_utf16.c_str(), 0, REG_SZ, (const BYTE*)value_utf16.data(), static_cast<DWORD>(value_utf16.size() * sizeof(UTF16_CHAR_TYPE)));
+				}
+			}
 		};
 
-		static Key CLASSES_ROOT;
-		static Key CURRENT_USER;
-		static Key LOCAL_MACHINE;
+		inline static const Key CLASSES_ROOT{ HKEY_CLASSES_ROOT };;
+		inline static const Key CURRENT_USER{ HKEY_CURRENT_USER };;
+		inline static const Key LOCAL_MACHINE{ HKEY_LOCAL_MACHINE };;
 	};
 }
+
+#endif
