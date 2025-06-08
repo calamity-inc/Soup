9904a2d9f69bac240f9181eb1e4700a5d668407a
Sainan <63328889+Sainan@users.noreply.github.com>
2025-06-08 10:50:49 +0200

    Add dnsResolver::makeDefault

--- soup/netConfig.cpp
+++ soup/netConfig.cpp
@@ -1,11 +1,5 @@
 #include "netConfig.hpp"
 
-#if SOUP_WASM
-#include "dnsHttpResolver.hpp"
-#else
-#include "dnsSmartResolver.hpp"
-#endif
-
 NAMESPACE_SOUP
 {
 	static thread_local netConfig netConfig_inst;
@@ -19,14 +13,7 @@ NAMESPACE_SOUP
 	{
 		if (!dns_resolver)
 		{
-#if SOUP_WASM
-			dns_resolver = soup::make_shared<dnsHttpResolver>();
-#else
-			// Reasons for not defaulting to dnsOsResolver:
-			// - Android doesn't have libresolv
-			// - Many ISPs provide disingenuous DNS servers, even blocking sites like pastebin.com
-			dns_resolver = soup::make_shared<dnsSmartResolver>();
-#endif
+			dns_resolver = dnsResolver::makeDefault();
 		}
 		return *dns_resolver;
 	}
