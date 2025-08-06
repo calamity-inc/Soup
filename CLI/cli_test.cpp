calamity-inc/Soup

commit 7ba990e3c20afc06fe98b535dcc353a74733b699

Update MimeMessage to handle multiple headers with the same key

soup/MimeMessage.cpp
@@ -8,8 +8,8 @@
 
 NAMESPACE_SOUP
 {
-	MimeMessage::MimeMessage(std::unordered_map<std::string, std::string>&& header_fields, std::string&& body)
-		: header_fields(std::move(header_fields)), body(std::move(body))
+	MimeMessage::MimeMessage(std::vector<std::string>&& headers, std::string&& body)
+		: headers(std::move(headers)), body(std::move(body))
 	{
 	}
 
@@ -26,9 +26,9 @@ NAMESPACE_SOUP
 
 	std::string MimeMessage::getBody() const
 	{
-		if (auto enc = header_fields.find(ObfusString("Content-Encoding")); enc != header_fields.end())
+		if (auto enc = findHeader(ObfusString("Content-Encoding")))
 		{
-			auto enc_joaat = joaat::hash(enc->second);
+			auto enc_joaat = joaat::hash(*enc);
 			switch (enc_joaat)
 			{
 			case joaat::hash("gzip"):
@@ -41,7 +41,7 @@ NAMESPACE_SOUP
 
 	void MimeMessage::setContentLength()
 	{
-		header_fields.emplace(ObfusString("Content-Length"), std::to_string(body.size()));
+		setHeader(ObfusString("Content-Length"), std::to_string(body.size()));
 	}
 
 	void MimeMessage::setContentType()
@@ -50,17 +50,17 @@ NAMESPACE_SOUP
 			&& body.substr(0, 4) == "\x89PNG"
 			)
 		{
-			header_fields.emplace("Content-Type", MimeType::IMAGE_PNG);
+			setHeader("Content-Type", MimeType::IMAGE_PNG);
 			return;
 		}
 
 		if (body.find("<body>") != std::string::npos)
 		{
-			header_fields.emplace("Content-Type", MimeType::TEXT_HTML);
+			setHeader("Content-Type", MimeType::TEXT_HTML);
 			return;
 		}
 
-		header_fields.emplace("Content-Type", MimeType::TEXT_PLAIN);
+		setHeader("Content-Type", MimeType::TEXT_PLAIN);
 	}
 
 	void MimeMessage::loadMessage(const std::string& data)
@@ -90,103 +90,135 @@ NAMESPACE_SOUP
 		}
 	}
 
-	bool MimeMessage::hasHeader(const std::string& key) const noexcept
+	Optional<std::string> MimeMessage::findHeader(std::string key) const SOUP_EXCAL
 	{
-#if SOUP_CPP20
-		return header_fields.contains(key);
-#else
-		return header_fields.find(key) != header_fields.end();
-#endif
-	}
-
-	std::string* MimeMessage::findHeader(std::string key) noexcept
-	{
-		string::lower(key);
-		for (auto& e : header_fields)
+		normaliseHeaderCasingInplace(key.data(), key.size());
+		for (const auto& header : headers)
 		{
-			if (string::lower(std::string(e.first)) == key)
+			if (header.size() > key.size()
+				&& header[key.size()] == ':'
+				&& memcmp(header.data(), key.data(), key.size()) == 0
+				)
 			{
-				return &e.second;
+				const auto value_off = key.size() + 2;
+				return std::string(header.data() + value_off, header.size() - value_off);
 			}
 		}
-		return nullptr;
+		return Optional<std::string>();
 	}
 
-	const std::string* MimeMessage::findHeader(std::string key) const noexcept
-	{
-		string::lower(key);
-		for (auto& e : header_fields)
-		{
-			if (string::lower(std::string(e.first)) == key)
-			{
-				return &e.second;
-			}
-		}
-		return nullptr;
-	}
-
-	void MimeMessage::addHeader(const std::string& line) SOUP_EXCAL
+	void MimeMessage::addHeader(std::string line) SOUP_EXCAL
 	{
 		if (auto key_offset = line.find(": "); key_offset != std::string::npos)
 		{
-			auto value = line.substr(key_offset + 2);
-			string::trim(value);
-			header_fields.emplace(normaliseHeaderCasing(line.substr(0, key_offset)), std::move(value));
+			normaliseHeaderCasingInplace(line.data(), key_offset);
+			headers.emplace_back(std::move(line));
 		}
 	}
 
-	void MimeMessage::setHeader(const std::string& _key, const std::string& value) SOUP_EXCAL
+	void MimeMessage::addHeader(std::string key, const std::string& value) SOUP_EXCAL
 	{
-		const auto key = normaliseHeaderCasing(_key);
-		if (auto e = header_fields.find(key); e != header_fields.end())
-		{
-			e->second = value;
-		}
-		else
-		{
-			header_fields.emplace(key, value);
-		}
+		normaliseHeaderCasingInplace(key.data(), key.size());
+		std::string line;
+		line.reserve(key.size() + 2 + value.size());
+		line.append(key);
+		line.append(": ");
+		line.append(value);
+		headers.emplace_back(std::move(line));
 	}
 
-	std::string MimeMessage::normaliseHeaderCasing(const std::string& key) SOUP_EXCAL
+	void MimeMessage::setHeader(std::string key, const std::string& value) SOUP_EXCAL
 	{
-		std::string out;
-		auto parts = string::explode(key, '-');
-		for (auto i = parts.begin(); i != parts.end(); ++i)
+		normaliseHeaderCasingInplace(key.data(), key.size());
+		for (auto& header : headers)
 		{
-			out.reserve(i->size() + 1);
-			out.push_back(string::upper_char(i->at(0)));
-			out.append(string::lower(i->substr(1)));
-			if ((i + 1) != parts.end())
+			if (header.size() > key.size()
+				&& header[key.size()] == ':'
+				&& memcmp(header.data(), key.data(), key.size()) == 0
+				)
 			{
-				out.push_back('-');
+				const auto value_off = key.size() + 2;
+				header = header.substr(0, value_off) + value;
+				return;
 			}
 		}
-		return out;
+		std::string line;
+		line.reserve(key.size() + 2 + value.size());
+		line.append(key);
+		line.append(": ");
+		line.append(value);
+		headers.emplace_back(std::move(line));
 	}
 
-	void MimeMessage::decode()
+	void MimeMessage::removeHeader(std::string key) noexcept
 	{
-		if (auto enc = header_fields.find(ObfusString("Content-Encoding")); enc != header_fields.end())
+		normaliseHeaderCasingInplace(key.data(), key.size());
+		for (auto i = headers.begin(); i != headers.end(); ++i)
 		{
-			auto enc_joaat = joaat::hash(enc->second);
+			const std::string& header = *i;
+			if (header.size() > key.size()
+				&& header[key.size()] == ':'
+				&& memcmp(header.data(), key.data(), key.size()) == 0
+				)
+			{
+				headers.erase(i);
+				break;
+			}
+		}
+	}
+
+	void MimeMessage::normaliseHeaderCasingInplace(char* data, size_t size) noexcept
+	{
+		bool first = true;
+		for (size_t i = 0; i != size; ++i)
+		{
+			if (first)
+			{
+				data[i] = string::upper_char(data[i]);
+			}
+			else
+			{
+				data[i] = string::lower_char(data[i]);
+			}
+			first = (data[i] == '-');
+		}
+	}
+
+	void MimeMessage::decode() SOUP_EXCAL
+	{
+		ObfusString enc_header_name("Content-Encoding");
+		if (auto enc = findHeader(enc_header_name))
+		{
+			auto enc_joaat = joaat::hash(*enc);
 			switch (enc_joaat)
 			{
 			case joaat::hash("gzip"):
 			case joaat::hash("deflate"):
-				header_fields.erase(enc);
+				removeHeader(enc_header_name);
 				body = deflate::decompress(body).decompressed;
 				break;
 			}
 		}
 	}
 
+	std::unordered_map<std::string, std::string> MimeMessage::getHeaderFields() const SOUP_EXCAL
+	{
+		std::unordered_map<std::string, std::string> map{};
+		for (const auto& header : headers)
+		{
+			const auto sep = header.find(": ");
+			SOUP_DEBUG_ASSERT(sep != std::string::npos);
+			map.emplace(header.substr(0, sep), header.substr(sep + 2));
+		}
+		return map;
+	}
+
 	std::string MimeMessage::toString() const
 	{
 		std::string res{};
-		for (const auto& field : header_fields)
+		for (const auto& header : headers)
 		{
-			res.append(field.first).append(": ").append(field.second).append("\r\n");
+			res.append(header).append("\r\n");
 		}
 		res.append("\r\n");
 		res.append(body);

CLI/cli_websrv.cpp
@@ -18,9 +18,9 @@ static std::unordered_map<std::string, soup::UniquePtr<soup::JitModule>> jit_mod
 
 static void handleRequest(soup::Socket& s, soup::HttpRequest&& req, soup::ServerWebService&)
 {
-	auto host = req.header_fields.find("Host");
-	if (host == req.header_fields.end()
-		|| host->second.empty()
+	auto host = req.findHeader("Host");
+	if (!host
+		|| host->empty()
 		)
 	{
 		std::cout << s.peer.toString() << " > " << req.method << " " << req.path << " [400 - no host]" << std::endl;
@@ -28,7 +28,7 @@ static void handleRequest(soup::Socket& s, soup::HttpRequest&& req, soup::Server
 		return;
 	}
 
-	std::string req_url = host->second;
+	std::string req_url = *host;
 	req_url.append(req.path);
 
 	if (req_url.find("..") != std::string::npos)
@@ -48,7 +48,7 @@ static void handleRequest(soup::Socket& s, soup::HttpRequest&& req, soup::Server
 		if (!std::filesystem::is_regular_file(file_path))
 		{
 			file_path = base_dir;
-			file_path.append(host->second);
+			file_path.append(*host);
 			file_path.append("/index.php");
 		}
 	}
@@ -85,7 +85,7 @@ static void handleRequest(soup::Socket& s, soup::HttpRequest&& req, soup::Server
 				t = soup::time::nanos() - t;
 				std::string timing = "PHP;dur=";
 				timing.append(std::to_string(t / 1000000.0));
-				resp.header_fields.emplace("Server-Timing", std::move(timing));
+				resp.setHeader("Server-Timing", std::move(timing));
 			}
 			else if (file_path.substr(file_path.length() - 4) == ".cpp")
 			{
@@ -135,7 +135,7 @@ static void handleRequest(soup::Socket& s, soup::HttpRequest&& req, soup::Server
 					}
 				}
 
-				resp.header_fields.emplace("Server-Timing", std::move(timing));
+				resp.setHeader("Server-Timing", std::move(timing));
 			}
 		}
 		soup::ServerWebService::sendContent(s, std::move(resp));

soup/HttpRequest.cpp
@@ -19,19 +19,19 @@
 
 NAMESPACE_SOUP
 {
-	HttpRequest::HttpRequest(std::string method, std::string host, std::string path)
+	HttpRequest::HttpRequest(std::string method, const std::string& host, std::string path)
 		: MimeMessage({
-			{ObfusString("Host"), std::move(host)},
-			{ObfusString("User-Agent"), ObfusString("Mozilla/5.0 (compatible; calamity-inc/Soup)")},
-			{ObfusString("Connection"), ObfusString("close")},
-			{ObfusString("Accept-Encoding"), ObfusString("deflate, gzip")},
+			{ObfusString("Host: ").str() + host},
+			{ObfusString("User-Agent: Mozilla/5.0 (compatible; calamity-inc/Soup)").str()},
+			{ObfusString("Connection: close").str()},
+			{ObfusString("Accept-Encoding: deflate, gzip").str()},
 		}), method(std::move(method)), path(std::move(path))
 	{
 		fixPath();
 	}
 
-	HttpRequest::HttpRequest(std::string host, std::string path)
-		: HttpRequest(ObfusString("GET"), std::move(host), std::move(path))
+	HttpRequest::HttpRequest(const std::string& host, std::string path)
+		: HttpRequest(ObfusString("GET"), host, std::move(path))
 	{
 	}
 
@@ -52,9 +52,9 @@ NAMESPACE_SOUP
 		}
 	}
 
-	const std::string& HttpRequest::getHost() const
+	std::string HttpRequest::getHost() const
 	{
-		return header_fields.at(ObfusString("Host"));
+		return findHeader(ObfusString("Host")).value();
 	}
 
 	std::string HttpRequest::getUrl() const
@@ -130,7 +130,7 @@ NAMESPACE_SOUP
 
 		HttpRequestExecuteData data{ this };
 		auto sock = make_shared<Socket>();
-		const auto& host = getHost();
+		const auto host = getHost();
 		if (sock->connect(host, port))
 		{
 			Scheduler sched{};
@@ -165,7 +165,7 @@ NAMESPACE_SOUP
 	{
 		HttpRequestExecuteEventStreamData data{ this, on_event, std::move(cap) };
 		auto sock = make_shared<Socket>();
-		const auto& host = getHost();
+		const auto host = getHost();
 		if (sock->connect(host, port))
 		{
 			Scheduler sched{};
@@ -230,12 +230,12 @@ NAMESPACE_SOUP
 
 	void HttpRequest::setClose() noexcept
 	{
-		header_fields.at(ObfusString("Connection")) = ObfusString("close").str();
+		setHeader(ObfusString("Connection"), ObfusString("close"));
 	}
 
 	void HttpRequest::setKeepAlive() noexcept
 	{
-		header_fields.at(ObfusString("Connection")) = ObfusString("keep-alive").str();
+		setHeader(ObfusString("Connection"), ObfusString("keep-alive"));
 	}
 
 	struct HttpResponseReceiver
@@ -337,19 +337,19 @@ NAMESPACE_SOUP
 						}
 						else
 						{
-							if (auto enc = self.resp.header_fields.find(ObfusString("Transfer-Encoding")); enc != self.resp.header_fields.end())
+							if (auto enc = self.resp.findHeader(ObfusString("Transfer-Encoding")))
 							{
-								if (joaat::hash(enc->second) == joaat::hash("chunked"))
+								if (joaat::hash(*enc) == joaat::hash("chunked"))
 								{
 									self.status = BODY_CHUNKED;
 								}
 							}
 							if (self.status == HEADER)
 							{
-								if (auto len = self.resp.header_fields.find(ObfusString("Content-Length")); len != self.resp.header_fields.end())
+								if (auto len = self.resp.findHeader(ObfusString("Content-Length")))
 								{
 									self.status = BODY_LEN;
-									if (auto opt = string::toIntOpt<uint64_t>(len->second, string::TI_FULL); opt.has_value())
+									if (auto opt = string::toIntOpt<uint64_t>(*len, string::TI_FULL); opt.has_value())
 									{
 										self.bytes_remain = opt.value();
 									}
@@ -367,9 +367,9 @@ NAMESPACE_SOUP
 								}
 								else
 								{
-									if (auto con = self.resp.header_fields.find(ObfusString("Connection")); con != self.resp.header_fields.end())
+									if (auto con = self.resp.findHeader(ObfusString("Connection")))
 									{
-										if (joaat::hash(con->second) == joaat::hash("close"))
+										if (joaat::hash(*con) == joaat::hash("close"))
 										{
 											self.status = BODY_CLOSE;
 											s.callback_recv_on_close = true;

soup/HttpRequest.hpp
@@ -20,11 +20,11 @@ NAMESPACE_SOUP
 		bool path_is_encoded = false;
 
 		HttpRequest() = default;
-		HttpRequest(std::string method, std::string host, std::string path);
-		HttpRequest(std::string host, std::string path);
+		HttpRequest(std::string method, const std::string& host, std::string path);
+		HttpRequest(const std::string& host, std::string path);
 		HttpRequest(const Uri& uri);
 
-		[[nodiscard]] const std::string& getHost() const;
+		[[nodiscard]] std::string getHost() const;
 		[[nodiscard]] std::string getUrl() const;
 
 		void setPath(std::string&& path);

soup/HttpRequestTask.cpp
@@ -247,7 +247,8 @@ NAMESPACE_SOUP
 			((HttpRequestTask*)fetch->userData)->setWorkDone();
 			emscripten_fetch_close(fetch);
 		};
-		for (const auto& field : hr.header_fields)
+		header_fields = hr.getHeaderFields();
+		for (const auto& field : header_fields)
 		{
 			if (field.first != "Host"
 				&& field.first != "User-Agent"

soup/HttpRequestTask.hpp
@@ -39,6 +39,7 @@ NAMESPACE_SOUP
 		SharedPtr<Socket> sock;
 		time_t awaiting_response_since;
 #else
+		std::unordered_map<std::string, std::string> header_fields;
 		std::vector<const char*> headers;
 #endif
 

soup/MimeMessage.hpp
@@ -2,19 +2,21 @@
 
 #include <string>
 #include <unordered_map>
+#include <vector>
 
 #include "base.hpp"
+#include "Optional.hpp"
 
 NAMESPACE_SOUP
 {
 	// This is \r\n line endings land.
 	struct MimeMessage
 	{
-		std::unordered_map<std::string, std::string> header_fields{};
+		std::vector<std::string> headers{};
 		std::string body{};
 
 		MimeMessage() = default;
-		MimeMessage(std::unordered_map<std::string, std::string>&& header_fields, std::string&& body = {});
+		MimeMessage(std::vector<std::string>&& headers, std::string&& body = {});
 		MimeMessage(const std::string& data);
 
 		void setBody(std::string body);
@@ -24,13 +26,17 @@ NAMESPACE_SOUP
 		void setContentType();
 
 		void loadMessage(const std::string& data);
-		[[nodiscard]] bool hasHeader(const std::string& key) const noexcept;
-		[[nodiscard]] std::string* findHeader(std::string key) noexcept;
-		[[nodiscard]] const std::string* findHeader(std::string key) const noexcept;
-		void addHeader(const std::string& line) SOUP_EXCAL;
-		void setHeader(const std::string& key, const std::string& value) SOUP_EXCAL;
-		[[nodiscard]] static std::string normaliseHeaderCasing(const std::string& key) SOUP_EXCAL;
-		void decode();
+		[[nodiscard]] bool hasHeader(std::string key) const SOUP_EXCAL { return findHeader(std::move(key)).has_value(); }
+		[[nodiscard]] Optional<std::string> findHeader(std::string key) const SOUP_EXCAL;
+		void addHeader(std::string line) SOUP_EXCAL;
+		void addHeader(std::string key, const std::string& value) SOUP_EXCAL;
+		void setHeader(std::string key, const std::string& value) SOUP_EXCAL;
+		void removeHeader(std::string key) noexcept;
+		static void normaliseHeaderCasingInplace(char* data, size_t size) noexcept;
+		void decode() SOUP_EXCAL;
+
+		// Provided for backwards compat but please note that the behaviour is undefined when multiple headers have the same key (e.g. Set-Cookie).
+		[[nodiscard]] std::unordered_map<std::string, std::string> getHeaderFields() const SOUP_EXCAL;
 
 		[[nodiscard]] std::string toString() const;
 

soup/ServerWebService.cpp
@@ -261,9 +261,9 @@ NAMESPACE_SOUP
 
 			if (srv.handle_request)
 			{
-				if (auto connection_entry = req.header_fields.find("Connection"); connection_entry != req.header_fields.end())
+				if (auto connection_entry = req.findHeader("Connection"))
 				{
-					if (connection_entry->second == "keep-alive")
+					if (*connection_entry == "keep-alive")
 					{
 						s.custom_data.getStructFromMap(WebServerClientData).keep_alive = true;
 					}

commit ed644fc3b4d39f1d87e8d96bd2d35df466b098c4

Fix regression in MimeMessage's handling of superfluous spaces Bug introduced in 7ba990e3c20afc06fe98b535dcc353a74733b699

soup/MimeMessage.cpp
@@ -112,6 +112,10 @@ NAMESPACE_SOUP
 		if (auto key_offset = line.find(": "); key_offset != std::string::npos)
 		{
 			normaliseHeaderCasingInplace(line.data(), key_offset);
+			for (size_t i = key_offset + 2; string::isSpace(line.c_str()[i]); )
+			{
+				line.erase(i, 1);
+			}
 			headers.emplace_back(std::move(line));
 		}
 	}

CLI/cli_test.cpp
@@ -65,6 +65,7 @@
 #include <EmailAddress.hpp>
 
 // net.web
+#include <MimeMessage.hpp>
 #include <Uri.hpp>
 
 // net
@@ -1580,6 +1581,18 @@ static void unit_net_email()
 	});
 }
 
+static void test_mime()
+{
+	{
+		MimeMessage mm("Transfer-Encoding: chunked\r\n\r\n");
+		assert(*mm.findHeader("Transfer-Encoding") == "chunked");
+	}
+	{
+		MimeMessage mm("Transfer-Encoding:  chunked\r\n\r\n");
+		assert(*mm.findHeader("Transfer-Encoding") == "chunked");
+	}
+}
+
 static void test_uri()
 {
 	std::string str;
@@ -1986,6 +1999,7 @@ void cli_test()
 			}
 			unit("web")
 			{
+				test("mime", &test_mime);
 				test("uri", &test_uri);
 			}
 			test("socket raii semantics", &test_socket_raii_semantics);