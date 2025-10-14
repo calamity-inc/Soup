00c0b71dc223d1262cb46d412fe811ca6514e156
Sainan <63328889+Sainan@users.noreply.github.com>
2025-10-14 11:33:47 +0200

    Make tunables fully templated

--- soup/tunables.hpp
+++ soup/tunables.hpp
@@ -1,70 +1,94 @@
 #pragma once
 
 #include <atomic>
+#include <optional>
+#include <unordered_map>
 
 #include "joaat.hpp"
 
 // Example usage:
 // static SOUP_TUNABLE(uint32_t, MY_TUNABLE) = 69;
-// soup::tunables::set("MY_TUNABLE", 420);
+// soup::tunables<uint32_t>::set("MY_TUNABLE", 420);
 
 #define SOUP_TUNABLE(T, name) ::soup::Tunable<T, ::soup::joaat::compileTimeHash(#name)> name
 
 NAMESPACE_SOUP
 {
+	template <typename T>
 	struct tunables
 	{
-		[[nodiscard]] static std::atomic<uint64_t>* find(const char* name) noexcept
+		// No mutexing as we're assuming that tunables are only registered in single-threaded static init, and we only read the map afterwards.
+		// Using optional instead of unordered_map directly because static init order is not guaranteed and unordered_map would require init of its own.
+		inline static std::optional<std::unordered_map<uint32_t, std::atomic<T>*>> s_tunables_map;
+
+		[[nodiscard]] static std::atomic<T>* find(const char* name) noexcept
 		{
 			return find(soup::joaat::hash(name));
 		}
 
-		[[nodiscard]] static std::atomic<uint64_t>* find(uint32_t name_joaat_hash) noexcept;
+		[[nodiscard]] static std::atomic<T>* find(uint32_t name_joaat_hash) noexcept
+		{
+			if (s_tunables_map)
+			{
+				if (auto e = s_tunables_map->find(name_joaat_hash); e != s_tunables_map->end())
+				{
+					return e->second;
+				}
+			}
+			return nullptr;
+		}
 
-		static void set(const char* name, uint64_t value) noexcept
+		static void set(const char* name, T value) noexcept
 		{
 			if (auto pValue = find(name))
 			{
-				pValue->store(value);
+				pValue->store(std::move(value));
 			}
 		}
 
-		static void set(uint32_t name_joaat_hash, uint64_t value) noexcept
+		static void set(uint32_t name_joaat_hash, T value) noexcept
 		{
 			if (auto pValue = find(name_joaat_hash))
 			{
-				pValue->store(value);
+				pValue->store(std::move(value));
 			}
 		}
 
-		static void internal_register(uint32_t name_joaat_hash, std::atomic<uint64_t>* pValue) noexcept;
+		static void internal_register(uint32_t name_joaat_hash, std::atomic<T>* pValue) noexcept
+		{
+			if (!s_tunables_map.has_value())
+			{
+				s_tunables_map.emplace();
+			}
+			s_tunables_map->emplace(name_joaat_hash, pValue);
+		}
 	};
 	
 
 	template <typename T, uint32_t name_joaat_hash>
 	struct Tunable
 	{
-		std::atomic<uint64_t> value;
+		std::atomic<T> value;
 
 		Tunable() noexcept
 		{
-			tunables::internal_register(name_joaat_hash, &value);
+			tunables<T>::internal_register(name_joaat_hash, &value);
 		}
 
 		Tunable(T default_value) noexcept
-			: value(static_cast<uint64_t>(default_value))
+			: value(std::move(default_value))
 		{
-			tunables::internal_register(name_joaat_hash, &value);
+			tunables<T>::internal_register(name_joaat_hash, &value);
 		}
 
 		void operator=(T value) noexcept
 		{
-			this->value.store(static_cast<uint64_t>(value));
+			this->value.store(std::move(value));
 		}
 
 		[[nodiscard]] operator T() const noexcept
 		{
-			return static_cast<T>(value.load());
+			return value.load();
 		}
 	};
 }
