d45da0a9e2691a81d73ddd7c3e4c4e8c367c59d9
Sainan <63328889+Sainan@users.noreply.github.com>
2025-10-16 16:42:31 +0200

    Require semicolon after SIG_INST macros

--- soup/pattern_macros.hpp
+++ soup/pattern_macros.hpp
@@ -3,5 +3,5 @@
 #include "CompiletimePattern.hpp"
 #include "CompiletimePatternWithOptBytes.hpp"
 
-#define SIG_INST(sig) constexpr ::soup::CompiletimePatternWithOptBytes<::soup::CompiletimePatternBase::countBytes(sig)> sig_inst(sig);
-#define SIMPLE_SIG_INST(sig) constexpr ::soup::CompiletimePattern<::soup::CompiletimePatternBase::countBytes(sig)> sig_inst(sig);
+#define SIG_INST(sig) constexpr ::soup::CompiletimePatternWithOptBytes<::soup::CompiletimePatternBase::countBytes(sig)> sig_inst(sig)
+#define SIMPLE_SIG_INST(sig) constexpr ::soup::CompiletimePattern<::soup::CompiletimePatternBase::countBytes(sig)> sig_inst(sig)
