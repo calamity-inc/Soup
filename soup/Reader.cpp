cfed8184e67ffb78cd520828531c638223d99ec2
Sainan <63328889+Sainan@users.noreply.github.com>
2025-10-25 10:52:11 +0200

    Optimise Reader::u64_dyn for x86-64 This is roughly 1.8x faster on my machine.

--- soup/Reader.cpp
+++ soup/Reader.cpp
@@ -1,10 +1,43 @@
 #include "Reader.hpp"
 
+#if SOUP_X86 && SOUP_BITS == 64
+#include <emmintrin.h> // _mm_movemask_epi8
+#include <immintrin.h> // _pext_u64
+
+#include "bitutil.hpp"
+#include "CpuInfo.hpp"
+#endif
+
 NAMESPACE_SOUP
 {
+#if defined(__GNUC__) || defined(__clang__)
+	__attribute__((target("bmi2")))
+#endif
 	bool Reader::u64_dyn(uint64_t& v) noexcept
 	{
 		v = 0;
+#if SOUP_X86 && SOUP_BITS == 64
+		if (CpuInfo::get().supportsSSE2() && CpuInfo::get().supportsBMI2())
+		{
+			uint64_t x;
+			if (raw(&x, sizeof(x)))
+			{
+				const auto pos = getPosition() - 8;
+				const auto byte_length = 1 + bitutil::getNumTrailingZeros(~(uint32_t)_mm_movemask_epi8(_mm_loadl_epi64((const __m128i*) & x)));
+				const uint64_t mask = ((byte_length < 8) * (1ull << (8 * byte_length))) - 1;
+				x = _pext_u64(x, 0x7f7f'7f7f'7f7f'7f7full & mask);
+				if (byte_length == 9)
+				{
+					uint64_t y;
+					SOUP_RETHROW_FALSE(raw(&y, 1));
+					x |= y << 56;
+				}
+				v = x;
+				seek(pos + byte_length);
+				return true;
+			}
+		}
+#endif
 		uint8_t b;
 		uint8_t bits = 0;
 		for (uint8_t i = 0; i != 8; ++i)
