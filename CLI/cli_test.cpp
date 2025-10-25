dd1697d0bd6dc0cc10cdb49cfd185025361c1799
Sainan <63328889+Sainan@users.noreply.github.com>
2025-10-25 14:27:49 +0200

    Add a test case for u64_dyn

--- CLI/cli_test.cpp
+++ CLI/cli_test.cpp
@@ -1364,6 +1364,7 @@ static void unit_io()
 			{ 0x80, "\x80\x00", 2 },
 			{ 1337, "\xB9\x09", 2 },
 			{ 42069, "\xD5\xC7\x01", 3 },
+			{ 0x123456789ABCDEF, "\xEF\x9A\xAE\xCC\xF7\xAB\xD0\x90\x00", 9 },
 			{ 0xffffffffffffffff, "\xFF\xFE\xFE\xFE\xFE\xFE\xFE\xFE\xFE", 9 },
 			{ 0x8000000000000000, "\x80\xFF\xFE\xFE\xFE\xFE\xFE\xFE\x7E", 9 },
 		};
