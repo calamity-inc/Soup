99bde6f17c354eb35afee14e234f43cf73afdade
Sainan <63328889+Sainan@users.noreply.github.com>
2026-01-27 09:39:28 +0100

    Scheduler: Callback socket waiting on data if unrecv_buf has data

--- soup/Scheduler.cpp
+++ soup/Scheduler.cpp
@@ -167,10 +167,17 @@ NAMESPACE_SOUP
 #if !SOUP_WASM
 		if (w.holdup_type == Worker::SOCKET)
 		{
-			pollfds.emplace_back(pollfd{
-				static_cast<Socket&>(w).fd,
-				POLLIN
-			});
+			if (!static_cast<Socket&>(w).unrecv_buf.empty())
+			{
+				fireHoldupCallback(w);
+			}
+			else
+			{
+				pollfds.emplace_back(pollfd{
+					static_cast<Socket&>(w).fd,
+					POLLIN
+				});
+			}
 		}
 		else
 #endif
