6954fdb39ddcbf835065901b6467a9e88f97c9bc
Sainan <63328889+Sainan@users.noreply.github.com>
2026-08-04 14:43:20 +0200

    Less hacky way to get float registers populated

--- soup/codegen/ffi.php
+++ soup/codegen/ffi.php
@@ -1,34 +1,58 @@
 <?php
 $max_args = 20;
 
-function call_case(int $args): string
+function call_case(string $argsarr, int $args, int $fargs): string
 {
 	$str = "\t\tcase ".$args.": return reinterpret_cast<uintptr_t(*)(";
+	for ($i = 0; $i != $fargs; ++$i)
+	{
+		if ($i != 0)
+		{
+			$str .= ", ";
+		}
+		$str .= "double";
+	}
 	for ($i = 0; $i != $args; ++$i)
 	{
-		if($i != 0)
+		if ($i != 0 || $fargs)
 		{
 			$str .= ", ";
 		}
 		$str .= "uintptr_t";
 	}
 	$str .= ")>(func)(";
-	for ($i = 0; $i != $args; ++$i)
+	for ($i = 0; $i != $fargs; ++$i)
 	{
-		if($i != 0)
+		if ($i != 0)
 		{
 			$str .= ", ";
 		}
-		$str .= "args[".$i."]";
+		$str .= "fargs[".$i."]";
+	}
+	for ($i = 0; $i != $args; ++$i)
+	{
+		if ($i != 0 || $fargs)
+		{
+			$str .= ", ";
+		}
+		$str .= $argsarr."[".$i."]";
 	}
 	$str .= ");\n";
 	return $str;
 }
 
-echo "\t\tswitch (nargs)\n";
+/*echo "\t\tswitch (nargs)\n";
 echo "\t\t{\n";
 for ($i = 0; $i != $max_args + 1; ++$i)
 {
-	echo call_case($i);
+	echo call_case("args", $i, 0);
+}
+echo "\t\t}\n";*/
+
+echo "\t\tswitch (npargs)\n";
+echo "\t\t{\n";
+for ($i = 0; $i != $max_args + 1; ++$i)
+{
+	echo call_case("pargs", $i, 8);
 }
 echo "\t\t}\n";
